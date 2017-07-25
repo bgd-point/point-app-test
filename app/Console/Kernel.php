5bb7094fa3ac9ed05cb9e7f7ca3b1fc62bd9a831
pringgojs <odyinggo@gmail.com>
2017-07-25 11:49:26 +0700

    console seed dummy
    add console seed data dummy

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends ConsoleKernel
         Commands\EmailApproval::class,
         Commands\ClearTransaction::class,
         Commands\ResetDatabase::class,
+        Commands\SeedDummy::class,
     ];
 
     /**
