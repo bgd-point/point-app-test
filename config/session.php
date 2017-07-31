c3d10c070b71f35ef83e3c8c65cded6cf974c15f
Martien Dermawan Tanama <martiendt@gmail.com>
2017-07-31 08:53:28 +0700

    change auto logout in 1 day

--- config/session.php
+++ config/session.php
@@ -29,9 +29,9 @@ return [
     |
     */
 
-    'lifetime' => 120,
+    'lifetime' => 1440,
 
-    'expire_on_close' => true,
+    'expire_on_close' => false,
 
     /*
     |--------------------------------------------------------------------------
