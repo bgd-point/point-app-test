5293716e804b3c8dd6287c7e3092d71411dd7f78
Martien Dermawan Tanama <martiendt@gmail.com>
2017-08-04 16:21:17 +0700

    update test barcode

--- resources/views/barcode.blade.php
+++ resources/views/barcode.blade.php
@@ -2,6 +2,7 @@
 
     .paper {
         width: 99px;
+        font-size: 11px;
     }
 
     .paper-block-1 {
@@ -23,13 +24,13 @@ $barcode = new DNS1D;
 <div class="paper">
     <span class="paper-block-1">
         <?php
-        echo $barcode->getBarcodeSVG("1234", "EAN13", 0.8);
+        echo $barcode->getBarcodeSVG("123456", "c128", 1);
         echo '<br> Test';
         ?>
     </span>
     <span class="paper-block-2">
         <?php
-        echo $barcode->getBarcodeSVG("1234", "EAN13", 0.8);
+        echo $barcode->getBarcodeSVG("123456", "C128", 1);
         echo '<br> Test';
         ?>
     </span>
