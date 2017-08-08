91b86e6374b4de4e11ed9fe5aefde875f8a1435a
pringgo <pringgo@point.red>
2017-08-08 15:52:13 +0700

    add log info jurnal

--- packages/point/point-framework/src/Helpers/JournalHelper.php
+++ packages/point/point-framework/src/Helpers/JournalHelper.php
@@ -253,7 +253,8 @@ class JournalHelper
         $journal = Journal::where('form_journal_id', $formulir_id)
             ->selectRaw('sum(debit) as debit, sum(credit) as credit')
             ->first();
-            
+
+        \Log::info('Debit : ' . $journal->debit . ' >><< Credit' .$journal->credit.);
         if ($journal->debit != $journal->credit) {
             throw new PointException('Journal unbalance, Please contact administrator to fix this error');
         }
