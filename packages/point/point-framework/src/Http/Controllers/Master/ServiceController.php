61351cbe336b24bfdffbb8826650e1ce02e0a96b
pringgo <pringgo@point.red>
2017-08-22 13:13:16 +0700

    fix service edit

--- packages/point/point-framework/src/Http/Controllers/Master/ServiceController.php
+++ packages/point/point-framework/src/Http/Controllers/Master/ServiceController.php
@@ -117,7 +117,7 @@ class ServiceController extends Controller
         access_is_allowed('update.service');
 
         $this->validate($request, [
-            'name' => 'required|unique:service',
+            'name' => 'required',
             'price' => 'required',
         ]);
 
