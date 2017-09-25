1f15ad45f9812b92fe623735382206f9af50197e
pringgo <pringgo@point.red>
2017-09-25 11:01:26 +0700

    jump to item when add row

--- packages/point/point-sales/src/views/app/sales/point/pos/edit.blade.php
+++ packages/point/point-sales/src/views/app/sales/point/pos/edit.blade.php
@@ -89,11 +89,11 @@
                         <tbody class="manipulate-row">
                             @for($i=0;$i<count($carts);$i++)
                             <?php $item = \Point\Framework\Models\Master\Item::find($carts[$i]['id']); ?>
-                            <tr >
+                            <tr>
                                 <td>
-                                <a href="javascript:void(0)" class="remove-row btn btn-danger" data-item="{{$carts[$i]['id']}}"><i class="fa fa-trash"></i></a></td>
+                                    <a id="row-item-{{$item->id}}" href="javascript:void(0)" class="remove-row btn btn-danger" data-item="{{$carts[$i]['id']}}"><i class="fa fa-trash"></i></a></td>
                                 <td style="vertical-align:middle">
-                                    <div style="margin-top:5px" id="item_name-{{$i}}">{{ $item->codeName }}</div>
+                                    <div style="margin-top:5px" id="item-name-{{$i}}">{{ $item->codeName }}</div>
                                     <input type="hidden" id="item-id-{{$i}}" name="item_id[]" value="{{$carts[$i]['id']}}"/>
                                 </td>
                                 <td>
@@ -429,8 +429,8 @@
     function addRow(result){
         var label = $("#unit_name_default").val();
         item_table.row.add( [
-            '<a href="javascript:void(0)" class="remove-row btn btn-danger" data-item="'+result.id+'"><i class="fa fa-trash"></i></a>',
-            '<div style="margin-top:5px" id="item_name-'+counter+'">'+result.item_name+'</div>'
+            '<a id="row-item-'+result.id+'" href="javascript:void(0)" class="remove-row btn btn-danger" data-item="'+result.id+'"><i class="fa fa-trash"></i></a>',
+            '<div style="margin-top:5px" id="item-name-'+counter+'">'+result.item_name+'</div>'
             +'<input type="hidden" id="item-id-'+counter+'" name="item_id[]" value="'+result.id+'"/>'
             +'<input type="hidden" name="old_quantity[]" id="old-quantity-'+counter+'" value="0">',
             '<div class="input-group">'
@@ -490,8 +490,8 @@
         }
         if(temps === true){ 
             for (var i = 0; i < counter; i++) {
-                if($('#item_name-'+i).length != 0){
-                    var item = $("#item_name-"+i).html();
+                if($('#item-name-'+i).length != 0){
+                    var item = $("#item-name-"+i).html();
                     if( item_name === item){
                         $("#item-quantity-"+i).val(quantity);
                         break;
@@ -506,6 +506,7 @@
         reloadItemHavingQuantity('#item-default');
         $("#item-quantity-default").val("0");
         $("#discount-default").val("0");
+        document.getElementById('row-item-' +result.id).scrollIntoView();
         document.getElementById('tax-choice-non-tax').checked = true;
     }
 
