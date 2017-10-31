9e32a90f3df2a197ab87a19b47a56effb2c77a52
Martien Dermawan Tanama <martiendt@gmail.com>
2017-10-31 11:01:17 +0700

    remove temporary cash advance in purchase

--- packages/point/point-purchasing/src/views/app/purchasing/point/inventory/purchase-requisition/create.blade.php
+++ packages/point/point-purchasing/src/views/app/purchasing/point/inventory/purchase-requisition/create.blade.php
@@ -69,13 +69,13 @@
                             @if(auth()->user()->may('create.supplier')) </div> @endif
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label class="col-md-3 control-label">Include Cash Advance</label>
-                        <div class="col-md-6 content-show">
-                            <input type="checkbox" id="include-cash-advance" name="include_cash_advance" value="true">
-                            <span class="help-block">Check for create cash advance</span>
-                        </div>
-                    </div>
+                    {{--<div class="form-group">--}}
+                        {{--<label class="col-md-3 control-label">Include Cash Advance</label>--}}
+                        {{--<div class="col-md-6 content-show">--}}
+                            <input type="hidden" id="include-cash-advance" name="include_cash_advance" value="false">
+                            {{--<span class="help-block">Check for create cash advance</span>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
                     <div class="form-group">
                         <label class="col-md-3 control-label">Notes</label>
                         <div class="col-md-6">
