7c4b2e43f50f625777c69117cf5c2f0eec151944
Martien Dermawan Tanama <martiendt@gmail.com>
2017-12-06 13:13:49 +0700

    fix ref

--- database/seeds/FixCashAdvanceSeeder.php
+++ database/seeds/FixCashAdvanceSeeder.php
@@ -12,11 +12,11 @@ class FixCashAdvanceSeeder extends Seeder
         \Log::info($cash_cash_advances->count());
 
         foreach($cash_cash_advances as $cash_cash_advance) {
-//            if ($cash_cash_advance->cashAdvance->payment_type == 'bank') {
-//                $cash_cash_advance->point_finance_cash_id = \Point\PointFinance\Models\Bank\Bank::find($cash_cash_advance->point_finance_cash_id)->formulir->id;
-//            } else {
-//                $cash_cash_advance->point_finance_cash_id = \Point\PointFinance\Models\Cash\Cash::find($cash_cash_advance->point_finance_cash_id)->formulir->id;
-//            }
+            if ($cash_cash_advance->cashAdvance->payment_type == 'bank') {
+                $cash_cash_advance->point_finance_cash_id = \Point\PointFinance\Models\Bank\Bank::find($cash_cash_advance->point_finance_cash_id)->formulir->id;
+            } else {
+                $cash_cash_advance->point_finance_cash_id = \Point\PointFinance\Models\Cash\Cash::find($cash_cash_advance->point_finance_cash_id)->formulir->id;
+            }
 
             \Log::info($cash_cash_advance->id);
             if ($cash_cash_advance->used->form_status == -1) {
