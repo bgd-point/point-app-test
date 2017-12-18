ed0ea12d66a4ec8356adc3e95151a51a59e2ca08
Martien Dermawan Tanama <martiendt@gmail.com>
2017-12-18 10:37:22 +0700

    fix layout email

--- packages/point/point-core/src/views/pdf/layout.blade.php
+++ packages/point/point-core/src/views/pdf/layout.blade.php
@@ -62,25 +62,17 @@
         @if(url_logo())
             <td>
                 <img src="{{ public_path('app/'.app('request')->project->url.'/logo/logo.png') }}" style="background-color:transparent;width:auto;height:50px;">
-                <div style="float:right">
-                    <span style="text-transform: uppercase; font-weight: bold">{{$warehouse->store_name ? : ''}}</span>
-                    <br/>
-                    {{$warehouse->address ? : ''}}
-                    <br>
-                    {{$warehouse->phone ? : ''}}
-                </div>
-            </td>
-        @else
-            <td>
-                <span style="text-transform: uppercase; font-weight: bold">{{$warehouse->store_name ? : ''}}</span>
-                <br/>
-                <span style="text-transform: capitalize;">
-                    {{$warehouse->address ? : ''}}
-                    <br/>
-                    {{$warehouse->phone ? : ''}}
-                </span>
             </td>
         @endif
+        <td>
+            <span style="text-transform: uppercase; font-weight: bold">{{$warehouse->store_name ? : ''}}</span>
+            <br/>
+            <span style="text-transform: capitalize;">
+                {{$warehouse->address ? : ''}}
+                <br/>
+                {{$warehouse->phone ? : ''}}
+            </span>
+        </td>
     </tr>
 </table>
 
