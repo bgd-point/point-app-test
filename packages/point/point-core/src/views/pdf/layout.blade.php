6fd574a2d773f8014b48569002ecad7b3362d1ee
Martien Dermawan Tanama <martiendt@gmail.com>
2017-12-18 10:31:09 +0700

    fix layout with logo

--- packages/point/point-core/src/views/pdf/layout.blade.php
+++ packages/point/point-core/src/views/pdf/layout.blade.php
@@ -61,7 +61,7 @@
     <tr>
         @if(url_logo())
             <td>
-                <img src="{{ public_path('app/'.app('request')->project->url.'/logo/logo.png') }}" style="float:right;background-color:transparent;width:auto;height:50px;">
+                <img src="{{ public_path('app/'.app('request')->project->url.'/logo/logo.png') }}" style="background-color:transparent;width:auto;height:50px;">
                 <div>
                     <span style="text-transform: uppercase; font-weight: bold">{{$warehouse->store_name ? : ''}}</span>
                     <br/>
