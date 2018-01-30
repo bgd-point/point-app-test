e32b402d502018b29062ca85b442ca8050585e50
Martien Dermawan Tanama <martiendt@gmail.com>
2018-01-30 13:40:49 +0700

    rename breadcrumb

--- packages/point/point-purchasing/src/views/app/purchasing/point/service/_breadcrumb.blade.php
+++ packages/point/point-purchasing/src/views/app/purchasing/point/service/_breadcrumb.blade.php
@@ -1,2 +1,2 @@
 <li><a href="{{ url('purchasing') }}">purchasing</a></li>
-<li><a href="{{ url('purchasing/point/service') }}">Service</a></li>
+<li><a href="{{ url('purchasing/point/service') }}">Service Purchase</a></li>
--- packages/point/point-sales/src/views/app/sales/point/service/_breadcrumb.blade.php
+++ packages/point/point-sales/src/views/app/sales/point/service/_breadcrumb.blade.php
@@ -1,2 +1,2 @@
 <li><a href="{{ url('sales') }}">Sales</a></li>
-<li><a href="{{ url('sales/point/service') }}">Service</a></li>
+<li><a href="{{ url('sales/point/service') }}">Service Sales</a></li>
