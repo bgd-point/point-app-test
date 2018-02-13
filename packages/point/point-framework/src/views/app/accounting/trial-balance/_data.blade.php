37e9669b55f60da3285cedf629f243cceb4c2346
Martien Dermawan Tanama <martiendt@gmail.com>
2018-02-13 15:08:46 +0700

    show minus report if account added in credit

--- packages/point/point-framework/src/views/app/accounting/trial-balance/_data.blade.php
+++ packages/point/point-framework/src/views/app/accounting/trial-balance/_data.blade.php
@@ -30,21 +30,26 @@
         $credit = \JournalHelper::coaCredit($coa->id, $date_from, $date_to);
         $total_credit += $credit;
         $ending_balance = \JournalHelper::coaEndingBalance($coa->id, $date_to);
+
+        if (!$coa->category->position->debit) {
+            $opening_balance *= -1;
+            $ending_balance *= -1;
+        }
     ?>
     <tr>
         <td>{{ $coa->account }}</td>
-        <td class="text-right">{{ $export ? $opening_balance : number_format_quantity($opening_balance) }}</td>
-        <td class="text-right">{{ $export ? $debit : number_format_quantity($debit) }}</td>
-        <td class="text-right">{{ $export ? $credit : number_format_quantity($credit) }}</td>
-        <td class="text-right">{{ $export ? $ending_balance : number_format_quantity($ending_balance) }}</td>
+        <td class="text-right">{{ $export ? $opening_balance : number_format_accounting($opening_balance) }}</td>
+        <td class="text-right">{{ $export ? $debit : number_format_accounting($debit) }}</td>
+        <td class="text-right">{{ $export ? $credit : number_format_accounting($credit) }}</td>
+        <td class="text-right">{{ $export ? $ending_balance : number_format_accounting($ending_balance) }}</td>
     </tr>
     @endforeach
     <tr>
         <td><strong>Total</strong></td>
-        <td class="text-right">{{ $export ? $total_opening_balance : number_format_quantity($total_opening_balance) }}</td>
-        <td class="text-right">{{ $export ? $total_debit : number_format_quantity($total_debit) }}</td>
-        <td class="text-right">{{ $export ? $total_credit : number_format_quantity($total_credit) }}</td>
-        <td class="text-right">{{ $export ? $total_ending_balance : number_format_quantity($total_ending_balance) }}</td>
+        <td class="text-right">{{ $export ? $total_opening_balance : number_format_accounting($total_opening_balance) }}</td>
+        <td class="text-right">{{ $export ? $total_debit : number_format_accounting($total_debit) }}</td>
+        <td class="text-right">{{ $export ? $total_credit : number_format_accounting($total_credit) }}</td>
+        <td class="text-right">{{ $export ? $total_ending_balance : number_format_accounting($total_ending_balance) }}</td>
     </tr>
     </tbody>
 </table>
