2ac3618dc0d023bc99fb31505bacf93fb56c9242
Martien Dermawan Tanama <martiendt@gmail.com>
2018-02-13 11:55:17 +0700

    fix typo

--- packages/point/point-expedition/src/views/app/expedition/point/expedition-order/_breadcrumb.blade.php
+++ packages/point/point-expedition/src/views/app/expedition/point/expedition-order/_breadcrumb.blade.php
@@ -1,2 +1,2 @@
 <li><a href="{{ url('expedition') }}">Expedition</a></li>
-<li><a href="{{ url('expedition/point/expedition-order') }}">Expedition Oder</a></li>
+<li><a href="{{ url('expedition/point/expedition-order') }}">Expedition Order</a></li>
