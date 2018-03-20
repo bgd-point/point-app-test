921354c9bf48f605621ce185498790a23e51a8b9
Martien Dermawan Tanama <martiendt@gmail.com>
2018-03-20 10:32:40 +0700

    prevent delete from used dp

--- packages/point/point-finance/src/Http/Controllers/PaymentController.php
+++ packages/point/point-finance/src/Http/Controllers/PaymentController.php
@@ -4,8 +4,10 @@ namespace Point\PointFinance\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Point\Framework\Helpers\FormulirHelper;
+use Point\Framework\Models\FormulirLock;
 use Point\PointFinance\Helpers\PaymentHelper;
 use Point\PointFinance\Models\PaymentReference;
+use Point\PointPurchasing\Models\Service\Downpayment;
 
 class PaymentController extends Controller
 {
@@ -20,6 +22,17 @@ class PaymentController extends Controller
         $permission_slug = app('request')->input('permission_slug');
         $formulir_id = app('request')->input('formulir_id');
 
+        $locks = FormulirLock::where('locking_id', $formulir_id)->where('locked', 1)->get();
+        foreach ($locks as $lock) {
+            if (strpos($lock->lockedForm->formulirable_type, 'Downpayment') !== false) {
+                $locks2 = FormulirLock::where('locked_id', $lock->locked_id)->where('locked', 1)->get();
+                if ($locks2->count() > 1) {
+                    gritter_error('Cannot delete this transaction');
+                    return array('status' => 'success');
+                }
+            }
+
+        }
         DB::beginTransaction();
 
         FormulirHelper::cancel($permission_slug, $formulir_id);
