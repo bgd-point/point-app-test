0797cb5d7b9cf1626eed3a0e62c24bcd89e66135
Martien Dermawan Tanama <martiendt@gmail.com>
2018-03-29 10:08:07 +0700

    update view pdf

--- packages/point/point-finance/src/views/app/finance/point/report/report-pdf.blade.php
+++ packages/point/point-finance/src/views/app/finance/point/report/report-pdf.blade.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>{{ucfirst($type)}} Report</title>
+    <title>{{strtoupper($type)}} REPORT</title>
     <link rel="stylesheet" href="{{asset('core/themes/appui-backend/css/bootstrap.min.css')}}">
 </head>
 <body>
-    <div class="h3 text-center">{{ucfirst($type)}} Report</div>
+    <div class="h3 text-center">{{strtoupper($type)}} REPORT</div>
     <div class="text-center"></div>
     <br>
     <table class="table tabble-striped table-bordered" cellpadding="0" cellspacing="0" border="0" >
         <thead>
             <tr>
-                <th>No. </th>    
-                <th>form date</th>
-                <th>form number</th>
-                <th>person</th>
-                <th>notes</th>
-                <th class="text-right">received</th>
-                <th class="text-right">disbursed</th>
+                <th>NO. </th>
+                <th>FORM DATE</th>
+                <th>FORM NUMBER <br/> VENDOR</th>
+                <th>NOTES</th>
+                <th class="text-right">RECEIVED</th>
+                <th class="text-right">DISBURSED</th>
             </tr>
         </thead>
         <tbody>
@@ -33,12 +32,11 @@
                     {{$i}}
                 </td>
                 <td>{{ date_format_view($report->formulir->form_date) }}</td>
-                <td>{{ $report->formulir->form_number}}</td>
-                <td>{{ strtoupper($report->person->name) }}</td>
+                <td>{{ $report->formulir->form_number}} <br/> <b>{{ strtoupper($report->person->name) }}</b></td>
                 <td>{{ strtoupper($report_detail->notes_detail) }}</td>
                 <td class="text-right">
                     @if($report->payment_flow == 'in')
-                    <b>{{ number_format_price($report_detail->amount) }}</b>
+                    <b>{{ number_format_price($report_detail->amount, 0) }}</b>
                     <?php $total_received += $report_detail->amount; ?>
                     @else
                     0.00
@@ -46,7 +44,7 @@
                 </td>
                 <td class="text-right">
                     @if($report->payment_flow == 'out')
-                    <b>{{ number_format_price($report_detail->amount) }}</b>
+                    <b>{{ number_format_price($report_detail->amount, 0) }}</b>
                     <?php $total_disbursed += $report_detail->amount * -1; ?>
                     @else
                     0.00
@@ -57,19 +55,19 @@
             @endforeach
         @endforeach  
         <tr>
-            <td colspan="5" class="text-right">Total</td>
-            <td class="text-right"><strong>{{ number_format_price($total_received) }}</strong></td>
-            <td class="text-right"><strong>{{ number_format_price($total_disbursed) }}</strong></td>
+            <td colspan="4" class="text-right">Total</td>
+            <td class="text-right"><strong>{{ number_format_price($total_received, 0) }}</strong></td>
+            <td class="text-right"><strong>{{ number_format_price($total_disbursed, 0) }}</strong></td>
         </tr>
         <tr>
-            <td colspan="5" class="text-right">Opening Balance</td>
-            <td class="text-right"><strong>{{ number_format_price($opening_balance) }}</strong></td>
+            <td colspan="4" class="text-right">Opening Balance</td>
+            <td class="text-right"><strong>{{ number_format_price($opening_balance, 0) }}</strong></td>
             <td></td>
         </tr>
         <tr>
-            <td colspan="5" class="text-right">Ending Balance</td>
+            <td colspan="4" class="text-right">Ending Balance</td>
             <td></td>
-            <td class="text-right"><strong>{{ number_format_price($opening_balance + $total_received + $total_disbursed) }}</strong></td>
+            <td class="text-right"><strong>{{ number_format_price($opening_balance + $total_received + $total_disbursed, 0) }}</strong></td>
         </tr>
         </tbody> 
     </table>
