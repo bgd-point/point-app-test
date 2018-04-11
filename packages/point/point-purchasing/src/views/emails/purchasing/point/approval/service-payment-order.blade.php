7c887c00c978352ced21f9ccbd8d56d50ab76801
Martien Dermawan Tanama <martiendt@gmail.com>
2018-04-11 16:50:36 +0700

    fix

--- packages/point/point-purchasing/src/views/emails/purchasing/point/approval/service-payment-order.blade.php
+++ packages/point/point-purchasing/src/views/emails/purchasing/point/approval/service-payment-order.blade.php
@@ -199,7 +199,7 @@
                             {{$invoice_service->service_notes}} (Qty: {{number_format_quantity($invoice_service->quantity)}})
                         </td>
                         <td style="text-align: right">
-                            {{number_format_quantity($payment_order_detail->amount)}}
+                            {{number_format_quantity($invoice_service->price * $invoice_service->quantity - ($invoice_service->price * $invoice_service->quantity * $invoice_service->discount / 100))}}
                         </td>
                     </tr>
                    @endforeach
