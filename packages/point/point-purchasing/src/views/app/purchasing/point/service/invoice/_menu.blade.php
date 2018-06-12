4a699517ad28c7a7cb4aebbc0bcdba1597565f2c
Christhofer Natalius <christhofer@gradin.co.id>
2018-06-12 11:32:58 +0700

    request approval button in purchase service invoice _menu, fixed button class using wrong segment link, removed segment check in temporary access button

--- packages/point/point-purchasing/src/views/app/purchasing/point/service/invoice/_menu.blade.php
+++ packages/point/point-purchasing/src/views/app/purchasing/point/service/invoice/_menu.blade.php
@@ -1,11 +1,13 @@
-<a href="{{ url('purchasing/point/service/invoice') }}" class="btn {{\Request::segment(4)==''?'btn-primary':'btn-info'}}">
+<a href="{{ url('purchasing/point/service/invoice') }}" class="btn {{\Request::segment(5)==''?'btn-primary':'btn-info'}}">
     List Data
 </a>
-<a href="{{ url('purchasing/point/service/invoice/create') }}" class="btn {{\Request::segment(4)==''?'btn-primary':'btn-info'}}">
+<a href="{{ url('purchasing/point/service/invoice/create') }}" class="btn {{\Request::segment(5)==''?'btn-primary':'btn-info'}}">
     Create
 </a>
-<a href="{{ url('temporary-access/invoice/point purchasing service invoice') }}" class="btn {{\Request::segment(4)=='access'?'btn-primary':'btn-info'}}">
+<a href="{{ url('temporary-access/invoice/point purchasing service invoice') }}" class="btn btn-info">
     Temporary Access
 </a>
-
+<a href="{{ url('purchasing/point/service/invoice/request-approval') }}" class="btn {{\Request::segment(5)=='request-approval'?'btn-primary':'btn-info'}}">
+    Request Approval
+</a>
 <br/><br/>
