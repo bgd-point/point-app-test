1a2846ab82327c60edb2a895b2145e839879625e
Christhofer Natalius <christhofer@gradin.co.id>
2018-06-26 10:36:46 +0700

    added insert in permission_role table for admin

--- database/seeds/TemporaryInsertSeeder.php
+++ database/seeds/TemporaryInsertSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use Symfony\Component\Console\Output\ConsoleOutput as Output;
 use Point\Core\Helpers\PermissionHelper;
+use Point\Core\Models\Master\PermissionRole;
+use Point\Core\Models\Master\Permission;
 
 class TemporaryInsertSeeder extends Seeder
 {
@@ -24,6 +26,15 @@ class TemporaryInsertSeeder extends Seeder
         $this->output->writeln('<info>--- Inserting export inventory value permission ---</info>');
 
         PermissionHelper::create('INVENTORY VALUE REPORT', ['export'], 'INVENTORY');
+        
+        $permission_export = Permission::where('slug', 'export.inventory.value.report')->first();
+        
+        $permission_role = new PermissionRole;
+
+        $permission_role->permission_id = $permission_export->id;
+        $permission_role->role_id = 1;
+
+        $permission_role->save();
 
         $this->output->writeln('<info>--- Insert export inventory value permission finished ---</info>');
 
