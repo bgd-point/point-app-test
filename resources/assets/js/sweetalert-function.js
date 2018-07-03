16888e54420356ccce7d4ce27aa7a3aecc60e16e
Christhofer Natalius <christhofer@gradin.co.id>
2018-07-03 16:30:23 +0700

    function secureRequestCancelForm now will disable button until ajax completed, need extra parameter

--- resources/assets/js/sweetalert-function.js
+++ resources/assets/js/sweetalert-function.js
@@ -87,8 +87,11 @@ function secureCancelForm(url, formulir_id, permission_slug)
     });
 }
 
-function secureRequestCancelForm(url, formulir_id, permission_slug)
+function secureRequestCancelForm(event, url, formulir_id, permission_slug)
 {
+    var spinner = ' <i class="fa fa-spinner fa-spin" style="font-size:16px;"></i>';
+    $(event).html(spinner).addClass('disabled');
+
     $.ajax({
         url: url,
         type: 'post',
@@ -99,6 +102,7 @@ function secureRequestCancelForm(url, formulir_id, permission_slug)
     })
     .always(function(data){
         notification(data['title'], data['msg']);
+        $(event).removeClass('disabled').html('REQUEST CANCEL FORM');
     });
 }
 
