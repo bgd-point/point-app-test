f551c00d8390b45d6d90ac93aac80063a964ead6
Christhofer Natalius <christhofer@gradin.co.id>
2018-07-08 09:07:56 +0700

    purchasing fixed assets purchase order create step 2 change radio button tax to checkbox, make checkbox tax also clickable by wrapping them inside label, removed non tax button and all its references

--- packages/point/point-purchasing/src/views/app/purchasing/point/fixed-assets/purchase-order/create-step-2.blade.php
+++ packages/point/point-purchasing/src/views/app/purchasing/point/fixed-assets/purchase-order/create-step-2.blade.php
@@ -199,18 +199,20 @@
                                     <tfoot>
                                     <tr>
                                         <td colspan="7" class="text-right">SUB TOTAL</td>
-                                        <td><input type="text" readonly id="subtotal"
-                                                   class="form-control format-quantity calculate text-right"
-                                                   onclick="setToNontax()" value="0"/></td>
+                                        <td><input type="text" readonly id="subtotal" value="0"
+                                                   class="form-control format-quantity calculate text-right" />
+                                        </td>
                                     </tr>
                                     <tr>
                                         <td colspan="7" class="text-right">DISCOUNT</td>
                                         <td>
-                                            <div class="input-group"><input type="text" id="discount" name="discount"
-                                                                            style="min-width: 100px"
-                                                                            class="form-control format-quantity calculate text-right"
-                                                                            value="{{old('discount') ? : 0}}"/><span
-                                                        class="input-group-addon">%</span>
+                                            <div class="input-group">
+                                                <input type="text" id="discount" name="discount"
+                                                       style="min-width: 100px"
+                                                       class="form-control format-quantity calculate text-right"
+                                                       value="{{old('discount') ? : 0}}"/>
+                                                <span class="input-group-addon">%</span>
+                                            </div>
                                         </td>
                                     </tr>
                                     <tr>
@@ -229,13 +231,19 @@
                                     <tr>
                                         <td colspan="7"></td>
                                         <td>
-                                            <input type="radio" id="tax-choice-include-tax" name="type_of_tax"
-                                                   {{ old('type_of_tax') == 'on' ? 'checked'  : '' }} onchange="calculate()"
-                                                   value="include"> Tax Included<br/>
-                                            <input type="radio" id="tax-choice-exclude-tax" name="type_of_tax"
-                                                   {{ old('type_of_tax') == 'on' ? 'checked'  : '' }} onchange="calculate()"
-                                                   value="exclude"> Tax Excluded <br/>
-                                            <input type="hidden" id="tax-choice-non-tax" name="type_of_tax" value="non">
+                                            <label>
+                                                <input type="checkbox" id="tax-choice-include-tax" name="type_of_tax"
+                                                   {{ old('type_of_tax') == 'include' ? 'checked'  : '' }}
+                                                   onchange="$('#tax-choice-exclude-tax').prop('checked', false); calculate();"
+                                                   value="include" /> Tax Included
+                                            </label>
+                                            <br />
+                                            <label>
+                                                <input type="checkbox" id="tax-choice-exclude-tax" name="type_of_tax"
+                                                   {{ old('type_of_tax') == 'exclude' ? 'checked'  : '' }}
+                                                   onchange="$('#tax-choice-include-tax').prop('checked', false); calculate();"
+                                                   value="exclude" /> Tax Excluded
+                                            </label>
                                         </td>
                                     </tr>
                                     <tr>
@@ -315,21 +323,6 @@
 
         $(function () {
             reloadPerson('#supplier-id', 'supplier', false);
-
-            $('#tax-choice-non-tax').hide();
-
-            var tax_status = {!! json_encode(old('tax_type')) !!};
-
-            if (tax_status == 'include') {
-                $("#tax-choice-include-tax").trigger("click");
-                $("#tax-choice-non-tax").val("include");
-            } else if (tax_status == 'exclude') {
-                $("#tax-choice-exclude-tax").trigger("click");
-                $("#tax-choice-non-tax").val("exclude");
-            } else {
-                $("#tax-choice-non-tax").val("non");
-            }
-
             calculate();
         });
 
@@ -340,13 +333,6 @@
                 $('#fee-expedition').val(0);
                 $('#fee-expedition').hide();
             }
-            calculate();
-        }
-
-        function setToNontax() {
-            $("#tax-choice-include-tax").attr("checked", false);
-            $("#tax-choice-exclude-tax").attr("checked", false);
-            $("#tax-choice-non-tax").val("non");
             calculate();
         }
 
@@ -384,13 +370,11 @@
 
             if ($('#tax-choice-exclude-tax').prop('checked')) {
                 tax = tax_base * 10 / 100;
-                $("#tax-choice-non-tax").val("exclude");
             }
 
             if ($('#tax-choice-include-tax').prop('checked')) {
                 tax_base = tax_base * 100 / 110;
                 tax = tax_base * 10 / 100;
-                $("#tax-choice-non-tax").val("include");
             }
 
             $('#tax_base').val(appNum(tax_base));
