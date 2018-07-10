f16f8b18ce0be782d6b067cad15b1a1605db3a79
Christhofer Natalius <christhofer@gradin.co.id>
2018-07-10 15:08:45 +0700

    purchasing inventory purchase order create step 2 changed radio button tax to checkbox, removed non tax button and all its reference

--- packages/point/point-purchasing/src/views/app/purchasing/point/inventory/purchase-order/create-step-2.blade.php
+++ packages/point/point-purchasing/src/views/app/purchasing/point/inventory/purchase-order/create-step-2.blade.php
@@ -204,9 +204,9 @@
                                     <tfoot>
                                     <tr>
                                         <td colspan="5" class="text-right">SUB TOTAL</td>
-                                        <td><input type="text" readonly id="subtotal"
-                                                   class="form-control format-quantity calculate text-right"
-                                                   onclick="setToNontax()" value="0"/></td>
+                                        <td><input type="text" readonly id="subtotal" value="0"
+                                                   class="form-control format-quantity calculate text-right" />
+                                        </td>
                                     </tr>
                                     <tr>
                                         <td colspan="5" class="text-right">DISCOUNT</td>
@@ -234,13 +234,19 @@
                                     <tr>
                                         <td colspan="5"></td>
                                         <td>
-                                            <input type="radio" id="tax-choice-include-tax" name="type_of_tax"
-                                                   {{ old('type_of_tax') == 'on' ? 'checked'  : '' }} onchange="calculate()"
-                                                   value="include"> Tax Included<br/>
-                                            <input type="radio" id="tax-choice-exclude-tax" name="type_of_tax"
-                                                   {{ old('type_of_tax') == 'on' ? 'checked'  : '' }} onchange="calculate()"
-                                                   value="exclude"> Tax Excluded <br/>
-                                            <input type="text" id="tax-choice-non-tax" name="type_of_tax" value="non">
+                                            <label>
+                                                <input type="checkbox" id="tax-choice-include-tax" name="type_of_tax"
+                                                   {{ old('type_of_tax') == 'include' ? 'checked'  : '' }}
+                                                   onchange="$('#tax-choice-exclude-tax').prop('checked', false); calculate();"
+                                                   value="include" /> Tax Included
+                                            </label>
+                                            <br/>
+                                            <label>
+                                                <input type="checkbox" id="tax-choice-exclude-tax" name="type_of_tax"
+                                                   {{ old('type_of_tax') == 'exclude' ? 'checked'  : '' }}
+                                                   onchange="$('#tax-choice-include-tax').prop('checked', false); calculate();"
+                                                   value="exclude" /> Tax Excluded
+                                            </label>
                                         </td>
                                     </tr>
                                     <tr>
@@ -321,21 +327,6 @@
 
         $(function () {
             reloadPerson('#contact_id', 'supplier', false);
-
-            $('#tax-choice-non-tax').hide();
-
-            var tax_status = {!! json_encode(old('type_of_tax')) !!};
-
-            if (tax_status == 'include') {
-                $("#tax-choice-include-tax").trigger("click");
-                $("#tax-choice-non-tax").val("include");
-            } else if (tax_status == 'exclude') {
-                $("#tax-choice-exclude-tax").trigger("click");
-                $("#tax-choice-non-tax").val("exclude");
-            } else {
-                $("#tax-choice-non-tax").val("non");
-            }
-
             calculate();
         });
 
@@ -346,13 +337,6 @@
                 $('#fee-expedition').val(0);
                 $('#fee-expedition').hide();
             }
-            calculate();
-        }
-
-        function setToNontax() {
-            $("#tax-choice-include-tax").attr("checked", false);
-            $("#tax-choice-exclude-tax").attr("checked", false);
-            $("#tax-choice-non-tax").val("non");
             calculate();
         }
 
@@ -389,13 +373,11 @@
 
             if ($('#tax-choice-exclude-tax').prop('checked')) {
                 tax = tax_base * 10 / 100;
-                $("#tax-choice-non-tax").val("exclude");
             }
 
             if ($('#tax-choice-include-tax').prop('checked')) {
                 tax_base = tax_base * 100 / 110;
                 tax = tax_base * 10 / 100;
-                $("#tax-choice-non-tax").val("include");
             }
 
             $('#tax_base').val(appNum(tax_base));
