47040e382d825ce854387add807801823aab077f
Christhofer Natalius <christhofer@gradin.co.id>
2018-07-24 23:42:02 +0700

    sales goods downpayment auto resend email approval, and fixed form link url in some email

--- packages/point/point-sales/src/views/app/emails/sales/point/approval/downpayment.blade.php
+++ packages/point/point-sales/src/views/app/emails/sales/point/approval/downpayment.blade.php
@@ -128,7 +128,7 @@
 
 <body>
 <div class="invoice-box">
-    Hi, you have an request approval downpayment from {{ $username }}. We would like to inform the
+    Hi, you have an request approval downpayment from <strong>{{ $requester }}</strong>. We would like to inform the
     details as follows :
 
    @foreach($list_data as $downpayment)
@@ -141,7 +141,20 @@
                     :
                 </td>
                 <td>
-                    {{ $downpayment->formulir->form_number }}</a>
+                    <a href="{{ $url . '/sales/point/indirect/downpayment/' . $downpayment->id }}">
+                        {{ $downpayment->formulir->form_number }}
+                    </a>
+                </td>
+            </tr>
+            <tr>
+                <td style="width: 20%">
+                    Created By
+                </td>
+                <td>
+                    :
+                </td>
+                <td>
+                    {{ $downpayment->formulir->createdBy->name }}
                 </td>
             </tr>
             <tr>
