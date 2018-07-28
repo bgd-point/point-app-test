5f78172f763158ec02d31245655c6cea51230b13
Christhofer Natalius <christhofer@gradin.co.id>
2018-07-28 12:07:54 +0700

    fixed error undefined variable because apparently not all pages send  to approval_status_label, added check if  exist

--- packages/point/point-framework/src/views/app/include/_approval_status_label.blade.php
+++ packages/point/point-framework/src/views/app/include/_approval_status_label.blade.php
@@ -1,7 +1,9 @@
 @if($approval_status == -1)
-    <label class="label label-danger" data-toggle="tooltip" data-original-title="{{date_format_view($approval_at, true)}}"><i class="fa fa-file-text"></i> Rejected</label>
+    <label class="label label-danger" data-toggle="tooltip" data-original-title="{{ !empty($approval_at) ? date_format_view($approval_at, true) : 'Approval Rejected' }}">
+    	<i class="fa fa-file-text"></i> Rejected</label>
 @elseif($approval_status == 0)
     <label class="label label-warning" data-toggle="tooltip" data-original-title="Approval Pending"><i class="fa fa-file-text"></i> Pending</label>
 @elseif($approval_status == 1)
-    <label class="label label-success" data-toggle="tooltip" data-original-title="{{date_format_view($approval_at, true)}}"><i class="fa fa-file-text"></i> Approved</label>
+    <label class="label label-success" data-toggle="tooltip" data-original-title="{{ !empty($approval_at) ? date_format_view($approval_at, true) : 'Approval Approved' }}">
+    	<i class="fa fa-file-text"></i> Approved</label>
 @endif
