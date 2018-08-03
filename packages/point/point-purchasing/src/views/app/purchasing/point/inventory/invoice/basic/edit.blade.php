ffaa22e441b4b329af1dd00774670cfe645500ab
Christhofer Natalius <christhofer@gradin.co.id>
2018-08-03 15:34:27 +0700

    goods purchase invoice basic

--- packages/point/point-purchasing/src/views/app/purchasing/point/inventory/invoice/basic/edit.blade.php
+++ packages/point/point-purchasing/src/views/app/purchasing/point/inventory/invoice/basic/edit.blade.php
@@ -172,13 +172,29 @@
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td colspan="6" class="text-right">DISCOUNT</td>
+                                        <td colspan="5" class="text-right">DISCOUNT</td>
                                         <td>
-                                            <div class="input-group"><input type="text" id="discount" name="discount"
-                                                                            class="form-control format-quantity calculate text-right"
-                                                                            style="min-width: 100px"
-                                                                            value="{{$invoice->discount}}"/><span
-                                                        class="input-group-addon">%</span></div>
+                                            <div class="input-group">
+                                                <span class="input-group-addon">Rp</span>
+                                                <input type="text"
+                                                       id="discount-rp"
+                                                       class="form-control text-right"
+                                                       style="min-width: 100px"
+                                                       onkeyup="discountNominalChanged()" 
+                                                       value="0"/>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="input-group">
+                                                <input type="text"
+                                                       id="discount"
+                                                       name="discount"
+                                                       maxlength="3"
+                                                       class="form-control calculate text-right"
+                                                       style="min-width: 100px"
+                                                       value="{{$invoice->discount}}"/>
+                                                <span class="input-group-addon">%</span>
+                                            </div>
                                         </td>
                                     </tr>
                                     <tr>
@@ -201,7 +217,7 @@
                                                    value="include"> Include Tax <br/>
                                             <input type="radio" id="tax-choice-exclude-tax" name="type_of_tax"
                                                    {{ $invoice_items->type_of_tax == 'exclude' ? 'checked'  : '' }} onchange="calculate()"
-                                                   value="exclude"> Exlude Tax <br/>
+                                                   value="exclude"> Exclude Tax <br/>
                                             <input type="hidden" id="tax-choice-non-tax" name="type_of_tax" {{ $invoice_items->type_of_tax == 'non' ? 'checked'  : '' }} value="non">
                                         </td>
                                     </tr>
@@ -345,20 +361,52 @@
             }
 
             $('#subtotal').val(appNum(subtotal));
+            calculateTotal();
+        }
 
-            if (dbNum($('#discount').val()) > 100) {
-                dbNum($('#discount').val(100))
+        $('#discount-rp').autoNumeric('init', {
+            vMin: '0', vMax: '999999999999.99', aPad: false, aSep: ',', aDec: '.'
+        });
+        function discountNominalChanged() {
+            var subtotal = dbNum($('#subtotal').val());
+
+            if(dbNum($("#discount-rp").val()) > subtotal)
+            {
+                $("#discount-rp").val(number_format(subtotal, 0, ".", ","));
+                $('#discount').val(100);
             }
+            else
+            {
+                var discount = dbNum($("#discount-rp").val()) / subtotal * 100
+                $('#discount').val(Number.parseFloat(discount.toFixed(15)));
+                // https://stackoverflow.com/questions/5037839/avoiding-problems-with-javascripts-weird-decimal-calculations
+            }
+            calculateTax();
+        }
 
+        function calculateTotal() {
             var subtotal = dbNum($('#subtotal').val());
             var discount = dbNum($('#discount').val());
-            if($('#tax-choice-include-tax').prop('checked')) {
-                $('#discount').val(0);
-                $('#discount').prop('readonly', true);
-                var discount = 0;
-            } else {
-                $('#discount').prop('readonly', false);
+
+            if (discount > 100) {
+                $('#discount').val(100);
+                $("#discount-rp").val(number_format(subtotal, 0, ".", ","));
             }
+            else {
+                $("#discount-rp").val(number_format(subtotal * discount / 100, 0, ".", ","));
+            }
+
+            if ($('#tax-choice-include-tax').prop('checked')) {
+                $('#discount, #discount-rp').val(0).prop('readonly', true);
+            } else {
+                $('#discount, #discount-rp').prop('readonly', false);
+            }
+            calculateTax();
+        }
+
+        function calculateTax(){
+            var discount = dbNum($('#discount').val());
+            var subtotal = dbNum($('#subtotal').val());
             var tax_base = subtotal - (subtotal / 100 * discount);
             $('#tax_base').val(appNum(tax_base));
             var tax = 0;
@@ -371,8 +419,6 @@
             if ($('#tax-choice-include-tax').prop('checked')) {
                 tax_base = tax_base * 100 / 110;
                 tax = tax_base * 10 / 100;
-                $('#tax_base').val(appNum(tax_base));
-
                 $("#tax-choice-non-tax").val("include");
             }
 
