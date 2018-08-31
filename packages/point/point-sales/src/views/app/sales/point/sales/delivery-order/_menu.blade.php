701b9f4342ac9a478250f08fec937bf1489341ff
Christhofer Natalius <christhofer@gradin.co.id>
2018-08-31 09:47:19 +0700

    fixed menu button not changed to purple when the page is active because wrong segment button

--- packages/point/point-sales/src/views/app/sales/point/sales/delivery-order/_menu.blade.php
+++ packages/point/point-sales/src/views/app/sales/point/sales/delivery-order/_menu.blade.php
@@ -1,16 +1,16 @@
 <a href="{{ url('sales/point/indirect/delivery-order') }}"
-   class="btn {{\Request::segment(4)==''?'btn-primary':'btn-info'}}">
+   class="btn {{\Request::segment(5)==''?'btn-primary':'btn-info'}}">
     List Data
 </a>
 <a href="{{ url('sales/point/indirect/delivery-order/create-step-1') }}"
-   class="btn {{\Request::segment(4)=='create-step-1' || \Request::segment(4)=='create-step-2'?'btn-primary':'btn-info'}}">
+   class="btn {{\Request::segment(5)=='create-step-1' || \Request::segment(5)=='create-step-2'?'btn-primary':'btn-info'}}">
     Create
 </a>
 <a href="{{ url('temporary-access/goods delivery/point sales delivery order') }}" class="btn btn-info">
     Temporary Access
 </a>
 <a href="{{ url('sales/point/indirect/delivery-order/request-approval') }}"
-   class="btn {{\Request::segment(4)=='request-approval'?'btn-primary':'btn-info'}}">
+   class="btn {{\Request::segment(5)=='request-approval'?'btn-primary':'btn-info'}}">
     Request Approval
 </a>
 
