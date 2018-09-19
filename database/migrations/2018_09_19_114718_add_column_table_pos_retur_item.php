182e9eb28012a4d2c3e99213c85780820bbc14b5
Christhofer Natalius <christhofer@gradin.co.id>
2018-09-19 11:53:01 +0700

    migration adding column add_stock and not_add_stock in pos sales return item

---
+++ database/migrations/2018_09_19_114718_add_column_table_pos_retur_item.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnTablePosReturItem extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('point_sales_pos_retur_item', function ($table) {
+            $table->decimal('add_stock', 16, 4);
+            $table->decimal('not_add_stock', 16, 4);
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('point_sales_pos_retur_item', function ($table) {
+            $table->dropColumn(['add_stock']);
+            $table->dropColumn(['not_add_stock']);
+        });
+    }
+}
