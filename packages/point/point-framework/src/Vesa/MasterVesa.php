e67c37be5edd1bf72cc0d8c6772f3d7b16b29fe4
Christhofer Natalius <christhofer@gradin.co.id>
2018-09-20 13:09:03 +0700

    fixed wrong stock

--- packages/point/point-framework/src/Vesa/MasterVesa.php
+++ packages/point/point-framework/src/Vesa/MasterVesa.php
@@ -63,72 +63,40 @@ class MasterVesa
 
     private static function vesaStockReminder($array = [], $merge_into_group = true)
     {
-        // $subQuery = \DB::table('inventory')
-        //                ->join('item as i', 'i.id', '=', 'inventory.item_id')
-        //                ->select(
-        //                    'item_id',
-        //                    'warehouse_id',
-        //                    'total_quantity',
-        //                    'i.reminder_quantity_minimum'
-        //                )
-        //                ->selectRaw('CONCAT("[", i.code, "] ", i.name) as item_code')
-        //                ->where('i.disabled', 0)
-        //                ->where('i.reminder', 1)
-        //                ->whereRaw('form_date = (
-        //                     SELECT MAX(form_date)
-        //                     FROM inventory ii
-        //                     WHERE ii.item_id = inventory.item_id AND ii.warehouse_id = inventory.warehouse_id
-        //                 )');
-        // $list_inventory = \DB::table(\DB::raw('('.$subQuery->toSql().') as o1'))
-        //             ->select('item_id', 'item_code', 'reminder_quantity_minimum')
-        //             ->selectRaw('SUM(total_quantity) as total_quantity')
-        //             ->having('total_quantity', '<=', 'reminder_quantity_minimum')
-        //             ->groupBy('item_id')
-        //             ->mergeBindings($subQuery)
-        //             ->get();
+        $items = Item::where('item.disabled', 0)->where('reminder', 1)->get();
 
-        $items = Inventory::rightJoin('item', 'item.id', '=', 'inventory.item_id')
-            ->where(\DB::raw('coalesce(inventory.total_quantity,0)'), '<', \DB::raw('item.reminder_quantity_minimum'))
-            ->where('item.reminder', 1)
-            ->where('item.disabled', 0)
-            ->select(
-                'item_id',
-                'warehouse_id',
-                'total_quantity',
-                'item.reminder_quantity_minimum'
-            )
-            ->selectRaw('CONCAT("[", item.code, "] ", item.name) as item_code')
-            ->orderBy('inventory.id', 'desc')
-            ->groupBy('inventory.item_id')
-            ->get();
-
-
-        if ($merge_into_group && count($items) > 5) {
-            array_push($array, [
-                'url' => url('master/item/stock-reminder'),
-                'deadline' => \Carbon::now(),
-                'message' => 'You have some items that need restock. Please restock them.',
-                'permission_slug' => 'create.point.purchasing.requisition'
-            ]);
-            return $array;
-        }
+        $date_from = $date_to = \Carbon::now();
+        $stockReminder = [];
 
         foreach ($items as $item) {
-            $unit = Item::defaultUnit($item->item_id);
+            $unit = Item::defaultUnit($item->id);
             $unit_name = $unit ? $unit->name : '';
-            $minimum_qty = number_format_quantity($item->reminder_quantity_minimum);
-            $current_qty = number_format_quantity($item->current_stock);
-            array_push($array, [
-                'url' => url('purchasing/point/purchase-order/basic/create'),
-                'deadline' => \Carbon::now(),
-                'message' => 'Please restock <strong>'.$item->item_code.'</strong>. 
-                              Minimum stock is <strong>' . $minimum_qty . ' ' . $unit_name . 
-                             '</strong>. Current stock is <strong>'. $current_qty . ' ' . $unit_name . '</strong>.',
-                'permission_slug' => 'create.point.purchasing.requisition'
-            ]);
-        }
+            $item_code = "[" . $item->code . "] " . $item->name;
+            $minimum_qty = $item->reminder_quantity_minimum;
+            $current_qty = inventory_get_closing_stock_all($date_from, $date_to, $item->id);
 
-        return $array;
+            if ($current_qty < $minimum_qty) {
+                if ($merge_into_group && count($stockReminder) === 5) {
+                    array_push($array, [
+                        'url' => url('master/item/stock-reminder'),
+                        'deadline' => \Carbon::now(),
+                        'message' => 'You have some items that need restock. Please restock them.',
+                        'permission_slug' => 'create.point.purchasing.requisition'
+                    ]);
+                    return $array;
+                }
+
+                array_push($stockReminder, [
+                    'url' => url('purchasing/point/purchase-order/basic/create'),
+                    'deadline' => \Carbon::now(),
+                    'message' => 'Please restock <strong>'.$item_code.'</strong>. 
+                              Minimum stock <strong>' . number_format_quantity($minimum_qty) . ' ' . $unit_name . 
+                             '</strong>. Current stock <strong>'. number_format_quantity($current_qty) . ' ' . $unit_name . '</strong>.',
+                    'permission_slug' => 'create.point.purchasing.requisition'
+                ]);
+            }
+        }
+        return array_merge($array, $stockReminder);
     }
 
     public static function vesaSetUserWarehouse($array = [])
