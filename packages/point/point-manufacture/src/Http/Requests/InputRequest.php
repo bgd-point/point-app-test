c140fcfb40029c248e2615fefbbf5f78191eb1dd
Martien Dermawan Tanama <martiendt@gmail.com>
2018-09-25 14:50:06 +0700

    temp fix 502 gateway

--- packages/point/point-manufacture/src/Http/Requests/InputRequest.php
+++ packages/point/point-manufacture/src/Http/Requests/InputRequest.php
@@ -41,54 +41,54 @@ class InputRequest extends Request
         ];
 
         // product items
-        for ($i = 0; $i < count(\Input::get('product_id')); $i++) {
-            if (! \Input::get('product_id')[$i]) {
-                $rules['[row_'.$i.']_product_required'] = 'accepted';
-            }
-            if (number_format_db(\Input::get('product_quantity')[$i]) == 0) {
-                $rules['[row_'.$i.']_product_quantity_required'] = 'accepted';
-            }
-            if (! \Input::get('product_warehouse_id')[$i]) {
-                $rules['[row_'.$i.']_product_warehouse_required'] = 'accepted';
-            }
-        }
+//        for ($i = 0; $i < count(\Input::get('product_id')); $i++) {
+//            if (! \Input::get('product_id')[$i]) {
+//                $rules['[row_'.$i.']_product_required'] = 'accepted';
+//            }
+//            if (number_format_db(\Input::get('product_quantity')[$i]) == 0) {
+//                $rules['[row_'.$i.']_product_quantity_required'] = 'accepted';
+//            }
+//            if (! \Input::get('product_warehouse_id')[$i]) {
+//                $rules['[row_'.$i.']_product_warehouse_required'] = 'accepted';
+//            }
+//        }
 
         // material items
-        for ($i = 0; $i < count(\Input::get('material_id')); $i++) {
-            $index = $i+1;
-            if (! \Input::get('material_id')[$i]) {
-                $rules['[row_'.$index.']_material_required'] = 'accepted';
-            }
-            if (number_format_db(\Input::get('material_quantity')[$i]) == 0) {
-                $rules['[row_'.$index.']_material_quantity_required'] = 'accepted';
-            }
-            if (! \Input::get('material_warehouse_id')[$i]) {
-                $rules['[row_'.$index.']_material_warehouse_required'] = 'accepted';
-            }
-
-            $date_to = \Input::get('form_date') ? date_format_db(\Input::get('form_date'),
-                "end") : date("Y-m-d 23:59:59");
-
-            if (\Input::get('action') == 'create') {
-                $max = inventory_get_available_stock($date_to,
-                \Input::get('material_id.' . $i),
-                \Input::get('material_warehouse_id.' . $i));
-                
-                if (number_format_db(\Input::get('material_quantity')[$i]) > $max) {
-                    $rules['raw_material_line_' . $index . '_greater_than_available_stock'] = 'accepted';
-                }
-            }
-
-            if (\Input::get('action') == 'edit') {
-                if (\Input::get('edit_notes') == '') {
-                    $rules['edit_notes_cannot_null'] = 'accepted';
-                }
-            }
-            
-            if (number_format_db(\Input::get('material_quantity')[$i]) < 1) {
-                $rules['raw_material_line_' . $index . '_cannot_null'] = 'accepted';
-            }
-        }
+//        for ($i = 0; $i < count(\Input::get('material_id')); $i++) {
+//            $index = $i+1;
+//            if (! \Input::get('material_id')[$i]) {
+//                $rules['[row_'.$index.']_material_required'] = 'accepted';
+//            }
+//            if (number_format_db(\Input::get('material_quantity')[$i]) == 0) {
+//                $rules['[row_'.$index.']_material_quantity_required'] = 'accepted';
+//            }
+//            if (! \Input::get('material_warehouse_id')[$i]) {
+//                $rules['[row_'.$index.']_material_warehouse_required'] = 'accepted';
+//            }
+//
+//            $date_to = \Input::get('form_date') ? date_format_db(\Input::get('form_date'),
+//                "end") : date("Y-m-d 23:59:59");
+//
+//            if (\Input::get('action') == 'create') {
+//                $max = inventory_get_available_stock($date_to,
+//                \Input::get('material_id.' . $i),
+//                \Input::get('material_warehouse_id.' . $i));
+//
+//                if (number_format_db(\Input::get('material_quantity')[$i]) > $max) {
+//                    $rules['raw_material_line_' . $index . '_greater_than_available_stock'] = 'accepted';
+//                }
+//            }
+//
+//            if (\Input::get('action') == 'edit') {
+//                if (\Input::get('edit_notes') == '') {
+//                    $rules['edit_notes_cannot_null'] = 'accepted';
+//                }
+//            }
+//
+//            if (number_format_db(\Input::get('material_quantity')[$i]) < 1) {
+//                $rules['raw_material_line_' . $index . '_cannot_null'] = 'accepted';
+//            }
+//        }
 
         return $rules;
     }
