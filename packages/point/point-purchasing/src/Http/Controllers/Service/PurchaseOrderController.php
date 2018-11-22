4c826a5450419c4843e75a9bf99dad0da01206fb
Christhofer Natalius <christhofer@gradin.co.id>
2018-11-22 14:34:25 +0700

    purchase order edit

--- packages/point/point-purchasing/src/Http/Controllers/Service/PurchaseOrderController.php
+++ packages/point/point-purchasing/src/Http/Controllers/Service/PurchaseOrderController.php
@@ -19,67 +19,123 @@ use Point\PointPurchasing\Http\Requests\ServicePurchaseOrderRequest;
 use Point\PointPurchasing\Models\Service\PurchaseOrder;
 
 class PurchaseOrderController extends Controller {
-  use ValidationTrait;
+    use ValidationTrait;
 
-  /**
-   * @return mixed
-   */
-  public function index() {
-    $view                      = view('point-purchasing::app.purchasing.point.service.purchase-order.index');
-    $list_purchase_order       = PurchaseOrder::joinFormulir()->notArchived()->with('person')->selectOriginal();
-    $list_purchase_order       = ServicePurchaseOrderHelper::searchList($list_purchase_order, \Input::get('order_by'), \Input::get('order_type'), \Input::get('status'), \Input::get('date_from'), \Input::get('date_to'), \Input::get('search'));
-    $view->list_purchase_order = $list_purchase_order->paginate(100);
+    /**
+     * @return mixed
+     */
+    public function index() {
+        $view = view('point-purchasing::app.purchasing.point.service.purchase-order.index');
 
-    $view->array_purchase_order_id = [];
+        $list_purchase_order = PurchaseOrder::joinFormulir()->notArchived()->with('person')->selectOriginal();
+        $list_purchase_order = ServicePurchaseOrderHelper::searchList($list_purchase_order, \Input::get('order_by'), \Input::get('order_type'), \Input::get('status'), \Input::get('date_from'), \Input::get('date_to'), \Input::get('search'));
 
-    return $view;
-  }
+        $view->list_purchase_order = $list_purchase_order->paginate(100);
 
-  /**
-   * @return mixed
-   */
-  public function create() {
-    $view                     = view('point-purchasing::app.purchasing.point.service.purchase-order.create');
-    $view->list_person        = PersonHelper::getByType(['supplier']);
-    $view->list_allocation    = Allocation::active()->get();
-    $view->list_user_approval = UserHelper::getAllUser();
-    $person_type              = PersonType::where('slug', 'supplier')->first();
-    $view->list_group         = PersonGroup::where('person_type_id', '=', $person_type->id)->get();
-    $view->code_contact       = PersonHelper::getCode($person_type);
+        $view->array_purchase_order_id = [];
 
-    return $view;
-  }
+        return $view;
+    }
 
-  /**
-   * @param ServicePurchaseOrderRequest $request
-   */
-  public function store(ServicePurchaseOrderRequest $request) {
-    DB::beginTransaction();
-    FormulirHelper::isAllowedToCreate('create.point.purchasing.service.purchase.order', date_format_db($request->input('form_date'), $request->input('time')), []);
+    /**
+     * @return mixed
+     */
+    public function create() {
+        $view                     = view('point-purchasing::app.purchasing.point.service.purchase-order.create');
+        $view->list_person        = PersonHelper::getByType(['supplier']);
+        $view->list_allocation    = Allocation::active()->get();
+        $view->list_user_approval = UserHelper::getAllUser();
 
-    $formulir       = FormulirHelper::create($request->input(), 'point-purchasing-service-purchase-order');
-    $purchase_order = ServicePurchaseOrderHelper::create($request, $formulir);
-    // timeline_publish('create.purchase-order', 'added new service purchase order ' . $purchase_order->formulir->form_number);
+        $person_type        = PersonType::where('slug', 'supplier')->first();
+        $view->list_group   = PersonGroup::where('person_type_id', '=', $person_type->id)->get();
+        $view->code_contact = PersonHelper::getCode($person_type);
 
-    DB::commit();
+        return $view;
+    }
 
-    gritter_success('create form success', 'false');
+    /**
+     * @param ServicePurchaseOrderRequest $request
+     */
+    public function store(ServicePurchaseOrderRequest $request) {
+        DB::beginTransaction();
+        FormulirHelper::isAllowedToCreate('create.point.purchasing.service.purchase.order', date_format_db($request->input('form_date'), $request->input('time')), []);
 
-    return redirect('purchasing/point/service/purchase-order/' . $purchase_order->id);
-  }
+        $formulir       = FormulirHelper::create($request->input(), 'point-purchasing-service-purchase-order');
+        $purchase_order = ServicePurchaseOrderHelper::create($request, $formulir);
+        // timeline_publish('create.purchase-order', 'added new service purchase order ' . $purchase_order->formulir->form_number);
 
-  /**
-   * @param  $id
-   * @return mixed
-   */
-  public function show($id) {
-    $view                               = view('point-purchasing::app.purchasing.point.service.purchase-order.show');
-    $view->purchase_order               = PurchaseOrder::find($id);
-    $view->list_purchase_order_archived = PurchaseOrder::joinFormulir()->archived($view->purchase_order->formulir->form_number)->selectOriginal()->get();
-    $view->revision                     = $view->list_purchase_order_archived->count();
-    $view->list_reference               = FormulirLock::where('locking_id', '=', $view->purchase_order->formulir_id)->where('locked', true)->get();
-    $view->email_history                = EmailHistory::where('formulir_id', $view->purchase_order->formulir_id)->get();
+        DB::commit();
 
-    return $view;
-  }
+        gritter_success('create form success', 'false');
+
+        return redirect('purchasing/point/service/purchase-order/' . $purchase_order->id);
+    }
+
+    /**
+     * @param  $id
+     * @return mixed
+     */
+    public function show($id) {
+        $view = view('point-purchasing::app.purchasing.point.service.purchase-order.show');
+
+        $view->purchase_order = PurchaseOrder::find($id);
+
+        $view->list_purchase_order_archived = PurchaseOrder::joinFormulir()->archived($view->purchase_order->formulir->form_number)->selectOriginal()->get();
+
+        $view->revision        = $view->list_purchase_order_archived->count();
+        $view->list_referenced = FormulirLock::where('locked_id', '=', $view->purchase_order->formulir_id)->where('locked', true)->get();
+        $view->email_history   = EmailHistory::where('formulir_id', $view->purchase_order->formulir_id)->get();
+
+        return $view;
+    }
+
+    /**
+     * @param  $id
+     * @return mixed
+     */
+    public function archived($id) {
+        $view = view('point-purchasing::app.purchasing.point.service.purchase-order.archived');
+
+        $view->purchase_order_archived = PurchaseOrder::find($id);
+
+        // $view->purchase_order = PurchaseOrder::joinFormulir()->notArchived($view->purchase_order_archived->formulir->archived)->selectOriginal()->first();
+
+        return $view;
+    }
+
+    /**
+     * @param  $id
+     * @return mixed
+     */
+    public function edit($id) {
+        $view                     = view('point-purchasing::app.purchasing.point.service.purchase-order.edit');
+        $view->purchase_order     = PurchaseOrder::find($id);
+        $view->person             = $view->purchase_order->person;
+        $view->list_user_approval = UserHelper::getAllUser();
+        $view->list_allocation    = Allocation::active()->get();
+
+        return $view;
+    }
+
+    /**
+     * @param ServicePurchaseOrderRequest $request
+     * @param $id
+     */
+    public function update(ServicePurchaseOrderRequest $request, $id) {
+        DB::beginTransaction();
+
+        $purchase_order = PurchaseOrder::find($id);
+        FormulirHelper::isAllowedToUpdate('update.point.purchasing.service.purchase.order', date_format_db($request->input('form_date'), $request->input('time')), $purchase_order->formulir);
+
+        $formulir_old   = FormulirHelper::archive($request->input(), $purchase_order->formulir_id);
+        $formulir       = FormulirHelper::update($request->input(), $formulir_old->archived, $formulir_old->form_raw_number);
+        $purchase_order = ServicePurchaseOrderHelper::create($request, $formulir);
+        // timeline_publish('update.purchase-order', 'update purchase-order service ' . $purchase_order->formulir->form_number);
+
+        DB::commit();
+
+        gritter_success('update form success', 'false');
+
+        return redirect('purchasing/point/service/purchase-order/' . $purchase_order->id);
+    }
 }
--- packages/point/point-purchasing/src/Models/Service/PurchaseOrder.php
+++ packages/point/point-purchasing/src/Models/Service/PurchaseOrder.php
@@ -6,53 +6,62 @@ use Illuminate\Database\Eloquent\Model;
 use Point\Core\Traits\ByTrait;
 use Point\Framework\Traits\FormulirTrait;
 
-class PurchaseOrder extends Model
-{
+class PurchaseOrder extends Model {
     use ByTrait, FormulirTrait;
 
+    /**
+     * @var string
+     */
     protected $table = 'point_purchasing_service_purchase_order';
+    /**
+     * @var mixed
+     */
     public $timestamps = false;
 
     /**
      * Inject function when saving
      *
-     * @param array $options
      *
+     * @param  array       $options
      * @return bool|null
      */
-    public function save(array $options = [])
-    {
+    public function save(array $options = []) {
         parent::save();
 
         $this->formulir->formulirable_type = get_class($this);
-        $this->formulir->formulirable_id = $this->id;
+        $this->formulir->formulirable_id   = $this->id;
         $this->formulir->save();
 
         return $this;
     }
 
-    public function person()
-    {
+    /**
+     * @return mixed
+     */
+    public function person() {
         return $this->belongsTo('\Point\Framework\Models\Master\Person', 'person_id');
     }
 
-    public function services()
-    {
+    /**
+     * @return mixed
+     */
+    public function services() {
         return $this->hasMany('\Point\PointPurchasing\Models\Service\PurchaseOrderDetail', 'purchase_order_id');
     }
 
-    public static function showUrl($id)
-    {
+    /**
+     * @param $id
+     */
+    public static function showUrl($id) {
         $class = self::find($id);
         if ($class->formulir->form_number) {
-            return '/purchasing/point/service/invoice/' . $id;
+            return '/purchasing/point/service/purchase-order/' . $id;
         } else {
-            return '/purchasing/point/service/invoice/' . $id . '/archived';
+            return '/purchasing/point/service/purchase-order/' . $id . '/archived';
         }
     }
 
-    public static function bladeEmail()
-    {
+    public static function bladeEmail() {
         return 'point-purchasing::emails.purchasing.point.approval.service-purchase-order';
     }
 }
