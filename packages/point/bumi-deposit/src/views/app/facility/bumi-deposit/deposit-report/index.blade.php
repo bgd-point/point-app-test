0064d7b10f8b7853861c7d89173d5a92cdac5778
Martien Dermawan Tanama <martiendt@gmail.com>
2019-01-11 11:05:02 +0700

    fix depo

--- packages/point/bumi-deposit/src/views/app/facility/bumi-deposit/deposit-report/index.blade.php
+++ packages/point/bumi-deposit/src/views/app/facility/bumi-deposit/deposit-report/index.blade.php
@@ -77,7 +77,9 @@
                         <th>Bank</th>
                         <th>Bank Account</th>
                         <th>No Bilyet</th>
-                        <th>Bank Interest</th>
+                        <th class="text-right">Bank Interest</th>
+                        <th class="text-right">Tax Interest</th>
+                        <th class="text-right">Total Interest</th>
                         <th class="text-right">Deposit Value</th>
                     </tr>
                     </thead>
@@ -85,11 +87,27 @@
                     @foreach($selected_group as $group)
                         @if($group->deposits(\Input::get('deposit_owner_id'), \Input::get('deposit_bank_id'), \Input::get('status'))->count())
                         <tr>
-                            <td colspan="8" class="themed-background-dark text-light-op">{{ $group->name }}</td>
+                            <td colspan="10" class="themed-background-dark text-light-op">{{ $group->name }}</td>
                         </tr>
-                        <?php $total_deposit = 0; ?>
+                        <?php
+                            $total_deposit = 0;
+                            $total_bank_interest = 0;
+                            $total_tax_interest = 0;
+                        ?>
                         @foreach($group->deposits(\Input::get('deposit_owner_id'), \Input::get('deposit_bank_id'), \Input::get('status'))->get() as $deposit)
-                            <?php $total_deposit += $deposit->original_amount; ?>
+                            <?php
+                                $bank_interest = 0;
+                                $tax_interest = 0;
+                                $total_interest = 0;
+                                if ($deposit->total_days_in_year > 0) {
+                                    $bank_interest = ($deposit->deposit_time * $deposit->original_amount * $deposit->interest_percent) / ($deposit->total_days_in_year * 100);
+                                    $tax_interest = $bank_interest - $deposit->total_interest;
+                                    $total_bank_interest += $bank_interest;
+                                    $total_tax_interest += $tax_interest;
+                                }
+                                $total_deposit += $deposit->original_amount;
+                                $total_interest += $deposit->total_interest;
+                            ?>
                             <tr>
                                 <td><a href="{{ url('facility/bumi-deposit/deposit/'.$deposit->id) }}">{{ $deposit->formulir->form_number }}</a></td>
                                 <td>{{ $deposit->owner->name }}</td>
@@ -97,12 +115,27 @@
                                 <td>{{ $deposit->bank->name }}</td>
                                 <td>{{ $deposit->bankAccount->account_number }} a/n {{ $deposit->bankAccount->account_name }}</td>
                                 <td>{{ $deposit->deposit_number }}</td>
-                                <td>{{ $deposit->interest_percent }} %</td>
+                                <td class="text-right">
+                                    {{ number_format_quantity($deposit->interest_percent) }} %
+                                    @if($deposit->total_days_in_year > 0)
+                                        ({{ number_format_quantity(($deposit->deposit_time * $deposit->original_amount * $deposit->interest_percent) / ($deposit->total_days_in_year * 100)) }})
+                                    @endif
+                                </td>
+                                <td class="text-right">
+                                    {{ number_format_quantity($deposit->tax_percent) }} %
+                                    @if($deposit->total_days_in_year > 0)
+                                        ({{ number_format_quantity((($deposit->deposit_time * $deposit->original_amount * $deposit->interest_percent) / ($deposit->total_days_in_year * 100)) - $deposit->total_interest) }})
+                                    @endif
+                                </td>
+                                <td class="text-right">{{ number_format_quantity($deposit->total_interest) }}</td>
                                 <td class="text-right">{{ number_format_quantity($deposit->original_amount) }}</td>
                             </tr>
                         @endforeach
                         <tr>
-                            <td colspan="7"></td>
+                            <td colspan="6"></td>
+                            <td class="text-right"><b>{{ number_format_quantity($total_bank_interest) }}</b></td>
+                            <td class="text-right"><b>{{ number_format_quantity($total_tax_interest) }}</b></td>
+                            <td class="text-right"><b>{{ number_format_quantity($total_interest) }}</b></td>
                             <td class="text-right"><b>{{ number_format_quantity($total_deposit) }}</b></td>
                         </tr>
                         @endif
