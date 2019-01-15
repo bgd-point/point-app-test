8a094e6b300a5e6fa6a091207437437b053cd4d9
Martien Dermawan Tanama <martiendt@gmail.com>
2019-01-15 10:27:21 +0800

    fix stock

--- app/Console/Commands/CheckInventoryCommand.php
+++ app/Console/Commands/CheckInventoryCommand.php
@@ -3,7 +3,11 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Point\Framework\Helpers\InventoryHelper;
+use Point\Framework\Helpers\JournalHelper;
 use Point\Framework\Models\Inventory;
+use Point\Framework\Models\Journal;
 
 class CheckInventoryCommand extends Command
 {
@@ -38,13 +42,68 @@ class CheckInventoryCommand extends Command
      */
     public function handle()
     {
-        $inventories = Inventory::all();
+        $manufactures = \Point\PointManufacture\Models\InputProcess::join('formulir', 'point_manufacture_input.formulir_id', '=', 'formulir.id')
+            ->where('formulir.form_date', '>', '2018-11-15 09:00:00')
+            ->whereNotNull('formulir.form_number')
+            ->where('process_id', 14)
+            ->select('point_manufacture_input.*')
+            ->get();
 
-        foreach ($inventories as $inventory) {
-            if ($inventory->formulir->form_number == null) {
-                $this->line('ketchup ' . $inventory->formulir->archived);
-                $inventory->delete();
+        foreach ($manufactures as $manufacture) {
+            DB::beginTransaction();
+            Journal::where('form_journal_id', $manufacture->formulir_id)->delete();
+
+            // Update InputMaterial
+            foreach ($manufacture->material as $material) {
+                $inventory = Inventory::where('formulir_id', $manufacture->formulir_id)->where('item_id', $material->material_id)->first();
+                if ($material->material_id == 42) {
+                    $material->material_id = 115;
+                    $inventory->item_id = 115;
+                } else if ($material->material_id == 9 || $material->material_id == 89) {
+                    $material->warehouse_id = 2;
+                    $inventory->warehouse_id = 2;
+                }
+
+                $cogs = InventoryHelper::getCostOfSales($manufacture->formulir->form_date,
+                    $material->material_id,
+                    $material->warehouse_id);
+
+                $material->cogs = $cogs * $material->quantity;
+                $material->save();
+
+                $inventory->price = $cogs;
+                $inventory->save();
+
+                // JOURNAL #1 of #2
+                $journal = new Journal();
+                $journal->form_date = $inventory->formulir->form_date;
+                $journal->coa_id = $inventory->item->account_asset_id;
+                $journal->description = 'Manufacture input process ' . $inventory->item->codeName;
+                $journal->debit = 0;
+                $journal->credit = abs($inventory->quantity * $inventory->price);
+                $journal->form_journal_id = $inventory->formulir->id;
+                $journal->form_reference_id;
+                $journal->subledger_id = $inventory->item_id;
+                $journal->subledger_type = get_class($inventory->item);
+                $journal->save();
+
+                // JOURNAL #2 of #2
+                $work_in_process_account_id = JournalHelper::getAccount('manufacture process', 'work in process');
+
+                $journal = new Journal();
+                $journal->form_date = $inventory->formulir->form_date;
+                $journal->coa_id = $work_in_process_account_id;
+                $journal->description = 'Manufacture input process ' . $inventory->item->codeName;
+                $journal->debit = abs($inventory->quantity * $inventory->price);
+                $journal->credit = 0;
+                $journal->form_journal_id = $inventory->formulir->id;
+                $journal->form_reference_id;
+                $journal->subledger_id;
+                $journal->subledger_type;
+                $journal->save();
             }
+
+            DB::commit();
         }
     }
 }
