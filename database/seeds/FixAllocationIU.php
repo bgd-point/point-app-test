b0516100235197d18cb507bb8a18d6f1cde889b4
Martien Dermawan Tanama <martiendt@gmail.com>
2019-02-02 18:12:34 +0800

    fix

--- database/seeds/FixAllocationIU.php
+++ database/seeds/FixAllocationIU.php
@@ -10,24 +10,32 @@ class FixAllocationIU extends Seeder
     {
         \DB::beginTransaction();
 
-        $salesReturns = \Point\PointSales\Models\Sales\Retur::all();
+//        $salesReturns = \Point\PointSales\Models\Sales\Retur::all();
+//
+//        foreach ($salesReturns as $return)
+//        {
+//            $ac = \Point\Framework\Models\AccountPayableAndReceivable::join('formulir',
+//                'formulir.id', '=', 'account_payable_and_receivable.formulir_reference_id')
+//                ->where('formulir.formulirable_type', \Point\PointSales\Models\Sales\Invoice::class)
+//                ->where('formulir.formulirable_id', $return->point_sales_invoice_id)
+//                ->select('account_payable_and_receivable.*')
+//                ->first();
+//
+//            $accountP = new \Point\Framework\Models\AccountPayableAndReceivableDetail();
+//            $accountP->form_date = $return->formulir->form_date;
+//            $accountP->account_payable_and_receivable_id = $ac->id;
+//            $accountP->formulir_reference_id = $return->formulir_id;
+//            $accountP->amount = $return->total;
+//            $accountP->notes = 'Retur';
+//            $accountP->save();
+//        }
 
-        foreach ($salesReturns as $return)
-        {
-            $ac = \Point\Framework\Models\AccountPayableAndReceivable::join('formulir',
-                'formulir.id', '=', 'account_payable_and_receivable.formulir_reference_id')
-                ->where('formulir.formulirable_type', \Point\PointSales\Models\Sales\Invoice::class)
-                ->where('formulir.formulirable_id', $return->point_sales_invoice_id)
-                ->select('account_payable_and_receivable.*')
-                ->first();
+        $as = \Point\Framework\Models\AccountPayableAndReceivable::where('done', 0)->get();
 
-            $accountP = new \Point\Framework\Models\AccountPayableAndReceivableDetail();
-            $accountP->form_date = $return->formulir->form_date;
-            $accountP->account_payable_and_receivable_id = $ac->id;
-            $accountP->formulir_reference_id = $return->formulir_id;
-            $accountP->amount = $return->total;
-            $accountP->notes = 'Retur';
-            $accountP->save();
+        foreach ($as as $a) {
+            if ($a->formulirReference->form_number == null) {
+                $a->delete();
+            }
         }
 
         \DB::commit();
