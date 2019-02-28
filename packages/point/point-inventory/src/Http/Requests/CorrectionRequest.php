f00f20f7d24794c6f862e8efe311cacc26dc704a
Martien Dermawan Tanama <martiendt@gmail.com>
2019-02-28 16:37:55 +0700

    fix error

--- packages/point/point-inventory/src/Http/Requests/CorrectionRequest.php
+++ packages/point/point-inventory/src/Http/Requests/CorrectionRequest.php
@@ -30,31 +30,31 @@ class CorrectionRequest extends Request
     public function rules()
     {
         $rules = [
-                    'form_date' => 'required',
-                    'warehouse_id' => 'required',
-                    'item_id' => 'required',
-                    'quantity_correction' => 'required',
-                    'correction_notes' => 'required',
-                    'approval_to' => 'required',
-                 ];
+            'form_date' => 'required',
+            'warehouse_id' => 'required',
+            'item_id' => 'required',
+            'quantity_correction' => 'required',
+            'correction_notes' => 'required',
+            'approval_to' => 'required',
+         ];
                  
         for ($i=0 ; $i<count(\Input::get('item_id')) ; $i++) {
-            $min = 1;
-            $j = $i+1;
-
-            if (\Input::get('quantity_correction.'.$i) < 0) {
-                $min = \Input::get('stock_exist.'.$i) * -1 ;
-            }
-
-            $rules['item_id.'.$i] = 'required';
-
-            if (number_format_db(\Input::get('quantity_correction')[$i]) == 0) {
-                $rules['[row_'.$j.']_quantity_correction_cannot_be_zero'] = 'accepted';
-            }
-
-            if (! \Input::get('correction_notes')[$i]) {
-                $rules['[row_'.$j.']_correction_notes_required'] = 'accepted';
-            }
+//            $min = 1;
+//            $j = $i+1;
+//
+//            if (\Input::get('quantity_correction.'.$i) < 0) {
+//                $min = \Input::get('stock_exist.'.$i) * -1 ;
+//            }
+//
+//            $rules['item_id.'.$i] = 'required';
+//
+//            if (number_format_db(\Input::get('quantity_correction')[$i]) == 0) {
+//                $rules['[row_'.$j.']_quantity_correction_cannot_be_zero'] = 'accepted';
+//            }
+//
+//            if (! \Input::get('correction_notes')[$i]) {
+//                $rules['[row_'.$j.']_correction_notes_required'] = 'accepted';
+//            }
         }
 
         return $rules ;
