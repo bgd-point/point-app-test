b6ff3d01f2b01e4571ed05b06af13006fb4efc01
Martien Dermawan Tanama <martiendt@gmail.com>
2019-03-21 16:41:04 +0700

    lock des 2018

--- packages/point/point-framework/src/Models/Formulir.php
+++ packages/point/point-framework/src/Models/Formulir.php
@@ -17,7 +17,7 @@ class Formulir extends Model
 
     public function save(array $options = [])
     {
-        $lockedDate = strtotime('2018-12-01');
+        $lockedDate = strtotime('2019-01-01');
 
         if (request()->get('database_name') == 'p_kbretail'
             && strtotime($this->form_date) < $lockedDate
