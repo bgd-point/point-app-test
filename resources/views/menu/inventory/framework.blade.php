c06e975726e0c0fea126e449df093d88df3444b7
Martien Dermawan Tanama <martiendt@gmail.com>
2019-03-21 21:00:30 +0700

    remove inv report standart

--- resources/views/menu/inventory/framework.blade.php
+++ resources/views/menu/inventory/framework.blade.php
@@ -1,13 +1,13 @@
 @if(\Auth::user()->may('read.inventory.report'))
-    <div class="col-md-4 col-lg-3">
-        <a href="{{url('inventory/report')}}" class="widget widget-button">
-            <div class="widget-content text-right clearfix">
-                <i class="gi fa-4x gi-book push-bit pull-left"></i>
-                <h4 class="widget-heading"><strong>Inventory Report</strong></h4>
-                <span class="text-muted"></span>
-            </div>
-        </a>
-    </div>
+    {{--<div class="col-md-4 col-lg-3">--}}
+        {{--<a href="{{url('inventory/report')}}" class="widget widget-button">--}}
+            {{--<div class="widget-content text-right clearfix">--}}
+                {{--<i class="gi fa-4x gi-book push-bit pull-left"></i>--}}
+                {{--<h4 class="widget-heading"><strong>Inventory Report</strong></h4>--}}
+                {{--<span class="text-muted"></span>--}}
+            {{--</div>--}}
+        {{--</a>--}}
+    {{--</div>--}}
 @endif
 @if(\Auth::user()->may('read.inventory.value.report'))
     <div class="col-md-4 col-lg-3">
