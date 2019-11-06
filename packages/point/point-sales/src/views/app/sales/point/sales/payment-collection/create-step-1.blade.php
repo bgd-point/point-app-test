f31706fc195ba2b004f51e666008f90b4c7f0e85
Martien Dermawan Tanama <martiendt@gmail.com>
2019-11-06 14:46:26 +0700

    update format

--- packages/point/point-sales/src/views/app/sales/point/sales/payment-collection/create-step-1.blade.php
+++ packages/point/point-sales/src/views/app/sales/point/sales/payment-collection/create-step-1.blade.php
@@ -19,41 +19,39 @@
                         <tr>
                             <th width="100px" class="text-center"></th>
                             <th>Customer</th>
-                            <th>Details</th>
+                            <th>Date</th>
+                            <th>Form Number</th>
+                            <th>Remaining</th>
                         </tr>
                         </thead>
                         <tbody>
                         <tr>
-                            <td colspan="3">From Invoice</td>
+                            <td colspan="6">From Invoice</td>
                         </tr>
                         @foreach($list_invoice as $invoice)
+                            <?php
+                            $list_invoice_by_person = \Point\PointSales\Models\Sales\Invoice::joinFormulir()
+                                ->joinPerson()
+                                ->notArchived()
+                                ->availableToPaymentCollection()
+                                ->where('person_id', '=', $invoice->person_id)
+                                ->selectOriginal()
+                                ->orderByStandard()
+                                ->get();
+                            ?>
+                            @foreach($list_invoice_by_person as $invoice_by_person)
                             <tr id="list-{{$invoice->formulir_id}}">
                                 <td class="text-center">
                                     <a href="{{ url('sales/point/indirect/payment-collection/create-step-2/'.$invoice->person_id) }}"
                                        class="btn btn-effect-ripple btn-xs btn-info"><i class="fa fa-external-link"></i>
                                         Payment Collection</a>
                                 </td>
-                                <td>
-                                    {!! get_url_person($invoice->person_id) !!}
-                                </td>
-                                <td>
-                                    <?php
-                                    $list_invoice_by_person = \Point\PointSales\Models\Sales\Invoice::joinFormulir()
-                                            ->joinPerson()
-                                            ->notArchived()
-                                            ->availableToPaymentCollection()
-                                            ->where('person_id', '=', $invoice->person_id)
-                                            ->selectOriginal()
-                                            ->orderByStandard()
-                                            ->get();
-                                    ?>
-                                    @foreach($list_invoice_by_person as $invoice_by_person)
-                                        {{date_format_view($invoice_by_person->formulir->form_date)}} <a
-                                                href="{{url('sales/point/indirect/invoice/'.$invoice_by_person->id)}}">{{$invoice_by_person->formulir->form_number}}</a>
-                                        <br/>
-                                    @endforeach
-                                </td>
+                                <td>{!! get_url_person($invoice->person_id) !!}</td>
+                                <td>{{date_format_view($invoice_by_person->formulir->form_date)}}</td>
+                                <td><a href="{{url('sales/point/indirect/invoice/'.$invoice_by_person->id)}}">{{$invoice_by_person->formulir->form_number}}</a></td>
+                                <td style="text-align: right">{{ number_format_price(\Point\Framework\Helpers\ReferHelper::remaining(get_class($invoice_by_person), $invoice_by_person->id, $invoice_by_person->total), 0) }}</td>
                             </tr>
+                            @endforeach
                         @endforeach
 
                         <tr>
@@ -70,13 +68,10 @@
                                    class="btn btn-effect-ripple btn-xs btn-info"><i class="fa fa-external-link"></i>
                                     Payment Collection</a>
                             </td>
-                            <td>
-                                {!! get_url_person($cut_off_receivable->person_id) !!}
-                            </td>
-                            <td>
-                                {{date_format_view($reference_receivable->formulir->form_date)}}
-                                <a href="{{url('accounting/point/cut-off/receivable/'.$reference_receivable->id)}}"> {{$reference_receivable->formulir->form_number}}</a>
-                            </td>
+                            <td>{!! get_url_person($cut_off_receivable->person_id) !!}</td>
+                            <td>{{date_format_view($reference_receivable->formulir->form_date)}}</td>
+                            <td><a href="{{url('accounting/point/cut-off/receivable/'.$reference_receivable->id)}}"> {{$reference_receivable->formulir->form_number}}</a></td>
+                            <td></td>
                         </tr>
                         @endforeach
                         </tbody>
