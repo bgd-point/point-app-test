0a545037a9cea77ebc2f1ebbfb94d3d92b63e216
Martien Dermawan Tanama <martiendt@gmail.com>
2019-11-06 11:52:58 +0700

    fix recalculate opname

--- app/Console/Commands/Recalculate.php
+++ app/Console/Commands/Recalculate.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Point\Framework\Helpers\InventoryHelper;
 use Point\Framework\Models\Inventory;
 use Point\Framework\Models\Master\Allocation;
 use Point\PointInventory\Models\StockOpname\StockOpname;
@@ -41,6 +42,62 @@ class Recalculate extends Command
             $alsd->save();
         }
 
+        $opnames = StockOpname::join('formulir', 'formulir.id', '=', 'point_inventory_stock_opname.formulir_id')
+            ->where('formulir.form_date', '>=', '2019-10-01')
+            ->where('formulir.form_date', '<=', '2019-10-02')
+            ->where('formulir.form_status', '>=', 1)
+            ->whereNotNull('formulir.form_number')
+            ->orderBy('formulir.form_date', 'asc')
+            ->select('point_inventory_stock_opname.*')
+            ->get();
+
+        foreach($opnames as $opname) {
+            foreach ($opname->items as $opnameItem) {
+                $inv = Inventory::where('inventory.item_id', $opnameItem->item_id)
+                    ->where('form_date', '<', $opname->formulir->form_date)
+                    ->where('warehouse_id', $opname->warehouse_id)
+                    ->orderBy('form_date', 'desc')
+                    ->first();
+
+                $stock = 0;
+
+                if ($inv) {
+                    $this->line('TOTAL QUANTITY ' . $inv->total_quantity);
+                    $stock = $inv->total_quantity;
+                }
+
+                $opnameItem->stock_in_database = $stock;
+                $opnameItem->save();
+            }
+
+            Inventory::where('formulir_id', $opname->formulir_id)->delete();
+
+            foreach ($opname->items as $opnameItem) {
+                $inventory = new Inventory;
+                $inventory->form_date = $opname->formulir->form_date;
+                $inventory->formulir_id = $opname->formulir_id;
+                $inventory->warehouse_id = $opname->warehouse_id;
+                $inventory->item_id = $opnameItem->item_id;
+                $inventory->price =  InventoryHelper::getCostOfSales($opname->formulir->form_date, $opnameItem->item_id, $opname->warehouse_id);
+                $quantity = $opnameItem->quantity_opname - $opnameItem->stock_in_database;
+                if ($quantity < 0) {
+                    $inventory->quantity = $quantity * -1;
+                    $inventory_helper = new InventoryHelper($inventory);
+                    $inventory_helper->out();
+                } elseif ($quantity > 0) {
+                    $inventory->quantity = $quantity;
+                    $inventory_helper = new InventoryHelper($inventory);
+                    $inventory_helper->in();
+                } else {
+                    continue;
+                }
+            }
+        }
+
+        \DB::commit();
+
+        \DB::beginTransaction();
+
         $inventories = Inventory::orderBy('form_date', 'asc')
             ->orderBy('formulir_id', 'asc')
             ->orderBy('id', 'asc')
@@ -54,7 +111,7 @@ class Recalculate extends Command
             $list_inventory = Inventory::with('formulir')
                 ->where('item_id', '=', $inventory->item_id)
                 ->where('warehouse_id', '=', $inventory->warehouse_id)
-//                ->where('form_date', '>=', '2019-08-01')
+                ->where('form_date', '>=', '2019-10-01')
                 ->orderBy('form_date', 'asc')
                 ->orderBy('formulir_id', 'asc')
                 ->orderBy('id', 'asc')
