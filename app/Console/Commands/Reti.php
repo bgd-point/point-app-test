5c61b9f1d29fd999d36265758194d603c6b71174
Martien Dermawan Tanama <martiendt@gmail.com>
2019-11-28 16:02:03 +0700

    fix reti

--- app/Console/Commands/Reti.php
+++ app/Console/Commands/Reti.php
@@ -92,6 +92,7 @@ class Reti extends Command
         $formulirs = Formulir::where('formulirable_type', '=', TransferItem::class)
             ->whereNotNull('form_number')
             ->whereNotNull('canceled_at')
+            ->where('approval_status', '=', 1)
             ->where('form_date', '>=', '2019-10-01 00:00:00')
             ->get();
 
