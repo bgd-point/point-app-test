04fa73d4cb5ceb9a159b88c868c96d88d8670711
Martien Dermawan Tanama <martiendt@gmail.com>
2020-01-23 15:14:05 +0700

    fix

--- packages/point/point-sales/src/views/app/sales/point/sales/payment-collection/periode.blade.php
+++ packages/point/point-sales/src/views/app/sales/point/sales/payment-collection/periode.blade.php
@@ -6,13 +6,23 @@
             @foreach ($invoices as $key => $invoice)
             <?php
                 $total = $invoice->total;
-                $debit = \Point\Framework\Models\Journal::where('form_reference_id', $invoice->formulir_id)->where('coa_id', 4)->sum('debit');
-                $credit = \Point\Framework\Models\Journal::where('form_reference_id', $invoice->formulir_id)->where('coa_id', 4)->sum('credit');
+                $debit = \Point\Framework\Models\Journal::where('form_reference_id', $invoice->formulir_id)
+                    ->where('form_date', '<' , '2020-01-01 00:00:00')->where('coa_id', 4)->sum('debit');
+                $credit = \Point\Framework\Models\Journal::where('form_reference_id', $invoice->formulir_id)
+                    ->where('form_date', '<' , '2020-01-01 00:00:00')->where('coa_id', 4)->sum('credit');
                 $piutang = $total - ($debit + $credit);
             ?>
+                <tr>
+                    <th>ID</th>
+                    <th>FORM</th>
+                    <th>DATE</th>
+                    <th>CUSTOMER</th>
+                    <th>INVOICE</th>
+                    <th>REMAINING</th>
+                </tr>
             @if ($piutang > 0)
                 <tr>
-                    <td>{{ $key }}</td>
+                    <td>{{ $key + 1 }}</td>
                     <td>{{ $invoice->formulir->form_number }}</td>
                     <td>{{ date('d F Y', strtotime($invoice->formulir->form_date)) }}</td>
                     <td>{{ $invoice->person->name }}</td>
