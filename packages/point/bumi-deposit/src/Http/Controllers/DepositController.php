e4af3d99ec59df5e57192fdfdc0fb754e9f725b1
Martien Dermawan Tanama <martiendt@gmail.com>
2020-02-26 11:49:40 +0700

    add role

--- packages/point/bumi-deposit/src/Http/Controllers/DepositController.php
+++ packages/point/bumi-deposit/src/Http/Controllers/DepositController.php
@@ -33,6 +33,7 @@ class DepositController extends Controller
         if (auth()->user()->id > 3) {
             $deposits = $deposits->where(function ($q) {
                 $q->where('bumi_deposit_group.name', 'BI')
+                    ->orWhere('bumi_deposit_group.name', 'AM')
                     ->orWhere('bumi_deposit_group.name', 'BNS')
                     ->orWhere('bumi_deposit_group.name', 'BIJ')
                     ->orWhere('bumi_deposit_group.name', 'BIM')
