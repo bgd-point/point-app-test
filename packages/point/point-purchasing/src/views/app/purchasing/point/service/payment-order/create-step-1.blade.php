6db5f5e5d3fa6ce2c73ca58fca9bf0ddc7cfccb3
Martien Dermawan Tanama <martiendt@gmail.com>
2020-09-01 12:08:13 +0700

    add more info

--- packages/point/point-purchasing/src/views/app/purchasing/point/service/payment-order/create-step-1.blade.php
+++ packages/point/point-purchasing/src/views/app/purchasing/point/service/payment-order/create-step-1.blade.php
@@ -19,11 +19,29 @@
                         <tr>
                             <th width="100px" class="text-center"></th>
                             <th>Supplier</th>
-                            <th>Info Invoices</th>
+                            <th>Invoice Date</th>
+                            <th>Invoice Number</th>
+                            <th>Description</th>
+                            <th>Amount</th>
                         </tr>
                         </thead>
                         <tbody>
                         @foreach($list_invoice as $invoice)
+                        <?php
+                        $list_invoice_by_person = \Point\PointPurchasing\Models\Service\Invoice::joinFormulir()
+                                ->joinPerson()
+                                ->notArchived()
+                                ->availableToPaymentOrder()
+                                ->where('person_id', '=', $invoice->person_id)
+                                ->selectOriginal()
+                                ->orderByStandard()
+                                ->get();
+                        ?>
+                        @foreach($list_invoice_by_person as $invoice_by_person)
+                            <?php
+                            $amount = number_format_price(\Point\Framework\Helpers\ReferHelper::remaining(get_class($invoice_by_person), $invoice_by_person->id, $invoice_by_person->total), 0);
+                            ?>
+                            @if ($amount > 0)
                             <tr id="list-{{$invoice->formulir_id}}">
                                 <td class="text-center">
                                     <a href="{{ url('purchasing/point/service/payment-order/create-step-2/'.$invoice->person_id) }}"
@@ -33,24 +51,22 @@
                                 <td>
                                     {!! get_url_person($invoice->person_id) !!}
                                 </td>
+                                <td>{{date_Format_view($invoice_by_person->formulir->form_date)}} </td>
                                 <td>
-                                    <?php
-                                    $list_invoice_by_person = \Point\PointPurchasing\Models\Service\Invoice::joinFormulir()
-                                            ->joinPerson()
-                                            ->notArchived()
-                                            ->availableToPaymentOrder()
-                                            ->where('person_id', '=', $invoice->person_id)
-                                            ->selectOriginal()
-                                            ->orderByStandard()
-                                            ->get();
-                                    ?>
-                                    @foreach($list_invoice_by_person as $invoice_by_person)
-                                        {{date_Format_view($invoice_by_person->formulir->form_date)}} <a
-                                                href="{{url('purchasing/point/service/invoice/'.$invoice_by_person->id)}}">{{$invoice_by_person->formulir->form_number}}</a>
-                                        <br/>
-                                    @endforeach
+                                    <a href="{{url('purchasing/point/service/invoice/'.$invoice_by_person->id)}}">{{$invoice_by_person->formulir->form_number}}</a>
                                 </td>
+                                <td>
+                                @foreach($invoice_by_person->services as $service)
+                                {{ $service->service_notes }}
+                                @if ($invoice_by_person->services->count() > 1)
+                                ,
+                                @endif
+                                @endforeach
+                                </td>
+                                <td>{{ $amount }}</td>
                             </tr>
+                            @endif
+                            @endforeach
                         @endforeach
                         </tbody>
                     </table>
