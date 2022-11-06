0fae318da6e71bf8150f20dfe534ad16409b208a
Martien Dermawan Tanama <martiendt@gmail.com>
2022-11-06 14:20:32 +0700

    Update Recalculate.php

--- app/Console/Commands/Recalculate.php
+++ app/Console/Commands/Recalculate.php
@@ -39,6 +39,7 @@ class Recalculate extends Command
 
         \DB::beginTransaction();
 
+        // Get all items
         $inventories = Inventory::orderBy('form_date', 'asc')
             ->get()
             ->unique(function ($inventory) {
@@ -46,114 +47,101 @@ class Recalculate extends Command
             });
 
         foreach ($inventories as $inventory) {
-$this->comment($inventory->id);
-            $count = 0;
             $list_inventory = Inventory::with('formulir')
                 ->where('item_id', '=', $inventory->item_id)
                 ->where('warehouse_id', '=', $inventory->warehouse_id)
                 ->where('form_date', '>=', '2022-01-01')
                 ->orderBy('form_date', 'asc')
                 ->get();
-$this->comment('1');
-            foreach ($list_inventory as $l_inventory) {
-$this->comment('1a');
-                $l_inventory->recalculate = false;
-                if ($l_inventory->formulir->formulirable_type === StockOpname::class) {
-                    $st = StockOpname::where('formulir_id', '=', $l_inventory->formulir->id)->first();
-                    $l_inventory->form_date = date('Y-m-d 23:59:59', strtotime($st->formulir->form_date));
-                    $l_inventory->save();
-                } else if ($l_inventory->quantity >= 0) {
-                    $l_inventory->form_date = date('Y-m-d 00:00:00', strtotime($l_inventory->form_date));
-                    $l_inventory->save();
-                } else {
-                    $l_inventory->form_date = date('Y-m-d 23:59:00', strtotime($l_inventory->form_date));
-                    $l_inventory->save();
-                }
-            }
-$this->comment('2');
+
             $total_quantity = 0;
             $total_value = 0;
             $cogs = 0;
             $cogs_tmp = 0;
+
             foreach ($list_inventory as $index => $l_inventory) {
-                // UPDATE QUANTITY IF FORMULIR TYPE IS STOCKOPNAME
-              if ($l_inventory->formulir->formulirable_type === StockOpname::class && $index > 0) {
-                    $stockopname = StockOpname::join('point_inventory_stock_opname_item', 'point_inventory_stock_opname.id', '=', 'point_inventory_stock_opname_item.stock_opname_id')
-                    ->where('point_inventory_stock_opname.formulir_id', $l_inventory->formulir_id)
-                    ->where('point_inventory_stock_opname_item.item_id', $l_inventory->item_id)
-                    ->select('point_inventory_stock_opname_item.*')
-                    ->first();
-                    $st = Inventory::where('item_id', $l_inventory->item_id)
-                        ->where('warehouse_id', $l_inventory->warehouse_id)
-                        ->where('form_date', '<', $l_inventory->form_date)
-                        ->orderBy('form_date', 'desc')
-                        ->orderBy('formulir_id', 'desc')
-                        ->orderBy('id', 'desc')
-                        ->first();
-                    if ($st && $stockopname->item_id == $l_inventory->item_id) {
-                        $sti = StockOpnameItem::where("id", $stockopname->id)->first();
-      $sti->stock_in_database = $st->total_quantity;
-                        $sti->save();
-                    }
-$this->comment('4a1');
-                    $total_quantity = $stockopname->quantity_opname;
-                   $this->comment('4a2');
- $l_inventory->total_quantity = $total_quantity;
-$this->comment('4a3');
-                    $l_inventory->quantity = $total_quantity - $list_inventory[$index-1]->total_quantity;
-$this->comment('4a4');
-                    $l_inventory->save();
+                if ($l_inventory->formulir->formulirable_type === StockOpname::class && $index > 0) {
+                    $this-updateQuantityOpname($l_inventory);
                 } else {
-                    // UPDATE TOTAL QUANTITY IF FORMULIR TYPE IS NOT STOCKOPNAME
-
-$this->comment('5');
-   if ($index == 0) {
-                        $inv = Inventory::where('inventory.item_id', $l_inventory->item_id)
-                            ->where('form_date', '<', $l_inventory->form_date)
-                            ->where('warehouse_id', $l_inventory->warehouse_id)
-                            ->orderBy('form_date', 'desc')
-                            ->orderBy('id', 'desc')
-                            ->first();
-                        if ($inv) {
-                            $total_quantity = $inv->total_quantity;
-                        } else {
-                            $total_quantity = 0;
-                        }
-                    }
-                    $total_quantity += $l_inventory->quantity;
-
-                    if ($l_inventory->quantity > 0) {
-                        // STOCK IN
-                        if ($total_quantity <= 0) {
-                            // IGNORE VALUE BECAUSE USER ERROR (STOCK MINUS)
-                            $total_value = 0;
-                            $cogs = 0;
-                        } else {
-                            $total_value += ($l_inventory->quantity * $l_inventory->price);
-                            $cogs = $total_value / $total_quantity;
-                        }
-                        $l_inventory->cogs = $cogs;
-                    } else {
-$this->comment('1000');
-                        // STOCK OUT
-                        if ($total_quantity < 0) {
-                            // STOCK MINUS = NEED FIX FROM USER
-                            $l_inventory->recalculate = true;
-                            $l_inventory->cogs = $cogs;
-                            $total_value = 0;
-                        } else {
-                            $total_value += ($l_inventory->quantity * $cogs);
-                            $l_inventory->cogs = $cogs;
-                        }
-                    }
-
-                    $l_inventory->total_quantity = $total_quantity;
-                    $l_inventory->total_value = $total_value;
-                    $l_inventory->save();
+                    $this->updateQuantityNonOpname($l_inventory, $index);
                 }
             }
         }
 
         \DB::commit();
+    }
+
+    private function updateQuantityOpname($l_inventory) {
+        // UPDATE QUANTITY IF FORMULIR TYPE IS STOCKOPNAME
+        $stockOpnameItem = StockOpname::join('point_inventory_stock_opname_item', 'point_inventory_stock_opname.id', '=', 'point_inventory_stock_opname_item.stock_opname_id')
+            ->where('point_inventory_stock_opname.formulir_id', $l_inventory->formulir_id)
+            ->where('point_inventory_stock_opname_item.item_id', $l_inventory->item_id)
+            ->select('point_inventory_stock_opname_item.*')
+            ->first();
+
+        // INVENTORY BEFORE STOCK OPNAME
+        $lastStock = Inventory::where('item_id', $l_inventory->item_id)
+            ->where('warehouse_id', $l_inventory->warehouse_id)
+            ->where('form_date', '<', $l_inventory->form_date)
+            ->orderBy('form_date', 'desc')
+            ->orderBy('formulir_id', 'desc')
+            ->orderBy('id', 'desc')
+            ->first();
+
+        // REPLACE STOCK IN DATABASE WITH CORRECT VALUES
+        if ($lastStock && $stockOpnameItem->item_id == $l_inventory->item_id) {
+            $sti = StockOpnameItem::where("id", $stockOpnameItem->id)->first();
+            $sti->stock_in_database = $lastStock->total_quantity;
+            $sti->save();
+        }
+
+        // UPDATE INVENTORIES TABLE
+        $total_quantity = $stockOpnameItem->quantity_opname;
+        $l_inventory->total_quantity = $total_quantity;
+        $l_inventory->quantity = $total_quantity - $list_inventory[$index-1]->total_quantity;
+        $l_inventory->save();
+    }
+
+    private function updateQuantityNonOpname($l_inventory, $index) {
+        // UPDATE TOTAL QUANTITY IF FORMULIR TYPE IS NOT STOCKOPNAME
+        if ($index == 0) {
+            $inv = Inventory::where('inventory.item_id', $l_inventory->item_id)
+                ->where('form_date', '<', $l_inventory->form_date)
+                ->where('warehouse_id', $l_inventory->warehouse_id)
+                ->orderBy('form_date', 'desc')
+                ->orderBy('id', 'desc')
+                ->first();
+            $total_quantity = $inv ? $inv->total_quantity : 0;
+        }
+        $total_quantity += $l_inventory->quantity;
+
+        if ($l_inventory->quantity > 0) {
+            // STOCK IN
+            if ($total_quantity <= 0) {
+                // IGNORE VALUE BECAUSE USER ERROR (STOCK MINUS)
+                $l_inventory->recalculate = true;
+                $total_value = 0;
+                $cogs = 0;
+            } else {
+                $total_value += ($l_inventory->quantity * $l_inventory->price);
+                $cogs = $total_value / $total_quantity;
+            }
+            $l_inventory->cogs = $cogs;
+        } else {
+            // STOCK OUT
+            if ($total_quantity < 0) {
+                // STOCK MINUS = NEED FIX FROM USER
+                $l_inventory->recalculate = true;
+                $l_inventory->cogs = $cogs;
+                $total_value = 0;
+            } else {
+                $total_value += ($l_inventory->quantity * $cogs);
+                $l_inventory->cogs = $cogs;
+            }
+        }
+
+        $l_inventory->total_quantity = $total_quantity;
+        $l_inventory->total_value = $total_value;
+        $l_inventory->save();
     }
 }
