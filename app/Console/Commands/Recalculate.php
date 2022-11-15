0946776e2500f556e4b8b881bc3b0ca089e2d664
Martien Dermawan Tanama <martiendt@gmail.com>
2022-11-16 03:35:08 +0700

    Update Recalculate.php

--- app/Console/Commands/Recalculate.php
+++ app/Console/Commands/Recalculate.php
@@ -60,7 +60,7 @@ class Recalculate extends Command
                 ->join('formulir', 'formulir.id', '=', 'point_inventory_stock_opname.formulir_id')
                 ->where('point_inventory_stock_opname_item.item_id', '=', $inventory->item_id)
                 ->where('point_inventory_stock_opname.warehouse_id', '=', $inventory->warehouse_id)
-                //->where('formulir.form_date', '<=', $inventory->form_date)
+                ->where('formulir.form_date', '<=', '2022-10-01')
                 ->where('formulir.form_status', '>=', 0)
                 ->where('formulir.approval_status', '>', 0)
                 ->whereNotNull('formulir.form_number')
