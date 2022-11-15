7b8f4625d54c262ae1daea902364ffa51c5af7b0
Martien Dermawan Tanama <martiendt@gmail.com>
2022-11-16 03:14:51 +0700

    Update Recalculate.php

--- app/Console/Commands/Recalculate.php
+++ app/Console/Commands/Recalculate.php
@@ -76,7 +76,7 @@ class Recalculate extends Command
                     ->get();
             }
             
-            $this->line($list_inventory->id);
+            $this->line($list_inventory[0]);
 
             $total_quantity = 0;
             $total_value = 0;
