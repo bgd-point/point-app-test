a27bbebd07d44cc89a7f82594584a05e796cd9fc
Martien Dermawan Tanama <martiendt@gmail.com>
2024-05-09 06:01:55 +0700

    s

--- packages/point/point-framework/src/views/app/menu/inventory/framework.blade.php
+++ packages/point/point-framework/src/views/app/menu/inventory/framework.blade.php
@@ -1,5 +1,5 @@
 @if(\Auth::user()->may('read.inventory.report'))
-    <div class="col-md-4 col-lg-3">
+    <!-- <div class="col-md-4 col-lg-3">
         <a href="{{url('inventory/report')}}" class="widget widget-button">
             <div class="widget-content text-right clearfix">
                 <i class="gi fa-4x gi-book push-bit pull-left"></i>
@@ -7,7 +7,7 @@
                 <span class="text-muted"></span>
             </div>
         </a>
-    </div>
+    </div> -->
 @endif
 @if(\Auth::user()->may('read.inventory.value.report'))
     <div class="col-md-4 col-lg-3">
