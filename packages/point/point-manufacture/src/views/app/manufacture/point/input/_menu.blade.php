6b46d8c41f99a4c052cc75db1ef6afe7c3f32565
Martien Dermawan Tanama <martiendt@gmail.com>
2024-05-10 14:19:54 +0700

    master

--- packages/point/point-manufacture/src/views/app/manufacture/point/input/_menu.blade.php
+++ packages/point/point-manufacture/src/views/app/manufacture/point/input/_menu.blade.php
@@ -6,10 +6,12 @@
    class="btn {{\Request::segment(6)=='choose-formula'?'btn-primary':'btn-info'}}">
     Choose Formula
 </a>
-<!-- <a href="{{ url('manufacture/point/process-io/'. $process->id .'/input/create') }}"
+<?php if(request()->get('database_name') == 'p_kbretail') { ?>
+<a href="{{ url('manufacture/point/process-io/'. $process->id .'/input/create') }}"
    class="btn {{\Request::segment(6)=='create'?'btn-primary':'btn-info'}}">
     Create
-</a> -->
+</a>
+<?php } ?>
 <a href="{{ url('temporary-access/manufacture | input/point manufacture input') }}" class="btn btn-info">
     Temporary Access
 </a>
