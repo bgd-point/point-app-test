00a4a5c0a4e95755826b6fe84abc2aafba31d45c
Martien Dermawan Tanama <martiendt@gmail.com>
2024-06-20 10:44:16 +0700

    x

--- packages/point/point-accounting/src/Http/Controllers/MemoJournalController.php
+++ packages/point/point-accounting/src/Http/Controllers/MemoJournalController.php
@@ -95,22 +95,22 @@ class MemoJournalController extends Controller
     {
         $coa_id = $request->input('coa_id');
         for ($i=0; $i<count($coa_id); $i++) {
-            \Log::info('mm1')
+            \Log::info('mm1');
             if ($coa_id[$i] == '') {
                 return false;
             }
 
             $coa = Coa::find($coa_id[$i]);
-            \Log::info('mm2')
+            \Log::info('mm2');
             if ($coa->has_subledger && $request->input('master')[$i] == '') {
                 return false;
             }
         }
-        \Log::info('mm3')
+        \Log::info('mm3');
         if ($request->input('foot_debit') == 0 || $request->input('foot_credit') == 0) {
             return false;
         }
-        \Log::info('mm4')
+        \Log::info('mm4');
         if (number_format_db($request->input('foot_debit')) != number_format_db($request->input('foot_credit'))) {
             return false;
         }
