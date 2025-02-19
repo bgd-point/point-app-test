e1f852157bf63b670b5d77535e4589f3b6bcc852
Martien Dermawan Tanama <martiendt@gmail.com>
2025-02-20 06:25:58 +0700

    ppn

--- packages/point/point-purchasing/src/views/app/purchasing/point/fixed-assets/purchase-order/create-step-2.blade.php
+++ packages/point/point-purchasing/src/views/app/purchasing/point/fixed-assets/purchase-order/create-step-2.blade.php
@@ -220,13 +220,6 @@
                                         </td>
                                     </tr>
                                     <tr>
-                                        <td colspan="7" class="text-right">TAX</td>
-                                        <td>
-                                            <input type="text" readonly="" id="tax"
-                                                   class="form-control format-quantity calculate text-right" value="0"/>
-                                        </td>
-                                    </tr>
-                                    <tr>
                                         <td colspan="7"></td>
                                         <td>
                                             <input type="radio" id="tax-choice-include-tax" name="type_of_tax"
@@ -236,6 +229,19 @@
                                                    {{ old('type_of_tax') == 'on' ? 'checked'  : '' }} onchange="calculate()"
                                                    value="exclude"> Tax Excluded <br/>
                                             <input type="hidden" id="tax-choice-non-tax" name="type_of_tax" value="non">
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="7" class="text-right">TAX PERCENTAGE</td>
+                                        <td>
+                                            <input type="text" id="tax-percentage" class="form-control format-quantity calculate text-right" value="11"/>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td colspan="7" class="text-right">TAX</td>
+                                        <td>
+                                            <input type="text" readonly="" id="tax"
+                                                   class="form-control format-quantity calculate text-right" value="0"/>
                                         </td>
                                     </tr>
                                     <tr>
@@ -383,13 +389,13 @@
             var tax = 0;
 
             if ($('#tax-choice-exclude-tax').prop('checked')) {
-                tax = tax_base * 11 / 100;
+                tax = tax_base * $('#tax-percentage').val() / 100;
                 $("#tax-choice-non-tax").val("exclude");
             }
 
             if ($('#tax-choice-include-tax').prop('checked')) {
-                tax_base = tax_base * 100 / 111;
-                tax = tax_base * 11 / 100;
+                tax_base = tax_base * 100 / (100 + $('#tax-percentage').val());
+                tax = tax_base * $('#tax-percentage').val() / 100;
                 $("#tax-choice-non-tax").val("include");
             }
 
