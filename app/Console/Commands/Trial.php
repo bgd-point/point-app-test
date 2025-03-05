0f87793bda42f3c4fa06a53e4a612bc5ab22013c
Martien Dermawan Tanama <martiendt@gmail.com>
2025-03-05 13:25:34 +0700

    x

---
+++ app/Console/Commands/Trial.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Point\Framework\Models\Journal;
+
+class Trial extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dev:trial';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check unbalance journal';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $journals = Journal::where('form_date', '>=', '2025-01-01')->get();
+
+        foreach ($journals as $journal) {
+            $debit = Journal::where('form_journal_id', '==', $journal->form_journal_id)->sum('debit');
+            $credit = Journal::where('form_journal_id', '==', $journal->form_journal_id)->sum('credit');
+            if ($debit !== $credit) {
+                $this->comment('ID ' . $journal->form_journal_id . ' | ' . $debit . ' / ' . $credit);
+            }
+        }
+    }
+}
