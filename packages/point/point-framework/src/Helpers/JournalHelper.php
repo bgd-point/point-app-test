1978eb4a5b3706149e13eac616716e9ca7d7b402
Martien Dermawan Tanama <martiendt@gmail.com>
2025-05-08 07:30:01 +0700

    x

--- packages/point/point-framework/src/Helpers/JournalHelper.php
+++ packages/point/point-framework/src/Helpers/JournalHelper.php
@@ -135,6 +135,7 @@ class JournalHelper
 
     public static function categoryValue($coa_category_id, $date_from, $date_to)
     {
+        \Log::info(coa_category_id);
         // RETAINED EARNING
         if ($coa_category_id == 13) {
             $coa_from_category = Coa::where('coa_category_id', '=', [14,15,16,17,18])->lists('coa.id');
