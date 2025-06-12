d0502b8be37ad1d770e4b1606ff556c6007e6736
Martien Dermawan Tanama <martiendt@gmail.com>
2025-06-12 11:51:45 +0700

    x

--- packages/point/point-accounting/src/Models/CutOffInventory.php
+++ packages/point/point-accounting/src/Models/CutOffInventory.php
@@ -47,8 +47,6 @@ class CutOffInventory extends Model
             ->orderBy('id', 'desc')
             ->first();
 
-        // dd($cutoff_inventory);
-
         if ($cutoff_inventory) {
             $cutoff_inventory = $cutoff_inventory->cutOffInventoryDetail
                 ->where('coa_id', $coa_id)
