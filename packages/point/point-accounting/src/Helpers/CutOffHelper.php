5cdbc6dd9ba14008aaf3ed881fdb8614334e32cb
Martien Dermawan Tanama <martiendt@gmail.com>
2025-06-12 07:05:12 +0700

    x

--- packages/point/point-accounting/src/Helpers/CutOffHelper.php
+++ packages/point/point-accounting/src/Helpers/CutOffHelper.php
@@ -37,7 +37,7 @@ class CutOffHelper
             ->notArchived()
             ->approvalApproved()
             ->open()
-            ->where('form_date', '=', $cut_off->formulir->created_at)
+            ->where('form_date', '=', $cut_off->formulir->form_date)
             ->orderby('formulir.id', 'desc')
             ->selectOriginal()
             ->first();
@@ -67,7 +67,7 @@ class CutOffHelper
     {
         foreach ($cut_off_account->cutOffAccountDetail as $cut_off_account_detail) {
             if ($cut_off_account_detail->coa->subledger_type == get_class(new FixedAsset())) {
-                $amount = CutOffFixedAssets::getSubledgerAmount($cut_off_account->formulir->created_at, $cut_off_account_detail->coa_id);
+                $amount = CutOffFixedAssets::getSubledgerAmount($cut_off_account->formulir->form_date, $cut_off_account_detail->coa_id);
                 $position = JournalHelper::position($cut_off_account_detail->coa_id);
                 
                 if (trim($cut_off_account_detail->$position) != trim($amount)) {
@@ -83,7 +83,7 @@ class CutOffHelper
     {
         foreach ($cut_off_account->cutOffAccountDetail as $cut_off_account_detail) {
             if ($cut_off_account_detail->coa->subledger_type == get_class(new Item())) {
-                $amount = CutOffInventory::getSubledgerAmount($cut_off_account->formulir->created_at, $cut_off_account_detail->coa_id);
+                $amount = CutOffInventory::getSubledgerAmount($cut_off_account->formulir->form_date, $cut_off_account_detail->coa_id);
                 $position = JournalHelper::position($cut_off_account_detail->coa_id);
                 $cut_off_amount = $cut_off_account_detail->$position;
                 
@@ -100,8 +100,8 @@ class CutOffHelper
     {
         foreach ($cut_off_account->cutOffAccountDetail as $cut_off_account_detail) {
             if ($cut_off_account_detail->coa->subledger_type == get_class(new Person())) {
-                $account_payable_amount = CutOffPayable::getSubledgerAmount($cut_off_account->formulir->created_at, $cut_off_account_detail->coa_id);
-                $account_receivable_amount = CutOffReceivable::getSubledgerAmount($cut_off_account->formulir->created_at, $cut_off_account_detail->coa_id);
+                $account_payable_amount = CutOffPayable::getSubledgerAmount($cut_off_account->formulir->form_date, $cut_off_account_detail->coa_id);
+                $account_receivable_amount = CutOffReceivable::getSubledgerAmount($cut_off_account->formulir->form_date, $cut_off_account_detail->coa_id);
                 $position = JournalHelper::position($cut_off_account_detail->coa_id);
                 if (trim($cut_off_account_detail->$position) == trim($account_payable_amount)
                     || trim($cut_off_account_detail->$position) == trim($account_receivable_amount)) {
@@ -118,7 +118,7 @@ class CutOffHelper
     private static function emptying($cut_off_account)
     {
         // INVENTORY
-        $inventories = Inventory::where('form_date', '<=', $cut_off_account->formulir->created_at)
+        $inventories = Inventory::where('form_date', '<=', $cut_off_account->formulir->form_date)
             ->select('inventory.*')
             ->groupBy('item_id')
             ->groupBy('warehouse_id')
@@ -128,7 +128,7 @@ class CutOffHelper
         if ($inventories) {
             foreach($inventories as $inventory) {
                 $emptying_inventory = new Inventory();
-                $emptying_inventory->form_date = $cut_off_account->formulir->created_at;
+                $emptying_inventory->form_date = $cut_off_account->formulir->form_date;
                 $emptying_inventory->formulir_id = $cut_off_account->formulir_id;
                 $emptying_inventory->warehouse_id = $inventory->warehouse_id;
                 $emptying_inventory->item_id = $inventory->item_id;
@@ -140,7 +140,7 @@ class CutOffHelper
 
                 $position = JournalHelper::position($inventory->item->account_asset_id);
                 $journal = new Journal();
-                $journal->form_date = date('Y-m-d H:i:s', strtotime($cut_off_account->formulir->created_at));
+                $journal->form_date = date('Y-m-d 23:59:59', strtotime($cut_off_account->formulir->form_date));
                 $journal->coa_id = $inventory->item->account_asset_id;
                 $journal->description = "Cut Off from formulir number " . $cut_off_account->formulir->form_number;
                 $journal->$position = $inventory->total_value * -1;
@@ -157,10 +157,10 @@ class CutOffHelper
         foreach ($list_account_payable_receivable as $account_payable_receivable) {
             $account_payable_receivable_detail_amount = AccountPayableAndReceivableDetail::where('account_payable_and_receivable_id', $account_payable_receivable->id)->sum('amount');
             $total_debt = $account_payable_receivable->amount - $account_payable_receivable_detail_amount;
-            if ($total_debt > 0 && $account_payable_receivable < $cut_off_account->formulir->created_at ) {
+            if ($total_debt > 0 && $account_payable_receivable < $cut_off_account->formulir->form_date ) {
                 $position = JournalHelper::position($account_payable_receivable->account_id);
                 $journal = new Journal();
-                $journal->form_date = date('Y-m-d H:i:s', strtotime($cut_off_account->formulir->created_at));
+                $journal->form_date = date('Y-m-d 23:59:59', strtotime($cut_off_account->formulir->form_date));
                 $journal->coa_id = $account_payable_receivable->account_id;
                 $journal->description = "Cut Off from formulir number ".$cut_off_account->formulir->form_number;
                 $journal->$position = $total_debt * -1;
@@ -177,12 +177,12 @@ class CutOffHelper
         // ACCOUNT NON SUBLEDGER
         $list_co_journal = Journal::joinCoa()->whereNull('coa.subledger_type')->get();
         foreach ($list_co_journal as $co_journal) {
-            $journal_value = JournalHelper::getTotalValue($co_journal->coa_id, $cut_off_account->formulir->created_at);
+            $journal_value = JournalHelper::getTotalValue($co_journal->coa_id, $cut_off_account->formulir->form_date);
             $position = JournalHelper::position($co_journal->coa_id);
             $total = abs($journal_value->debit - $journal_value->credit);
             if ($total > 0) {
                 $journal = new Journal();
-                $journal->form_date = date('Y-m-d H:i:s', strtotime($cut_off_account->formulir->created_at));
+                $journal->form_date = date('Y-m-d 23:59:59', strtotime($cut_off_account->formulir->form_date));
                 $journal->coa_id = $co_journal->coa_id;
                 $journal->description = "Cut Off from formulir number " . $cut_off_account->formulir->form_number;
                 $journal->$position = $total * -1;
@@ -199,7 +199,7 @@ class CutOffHelper
             ->approvalApproved()
             ->open()
             ->notArchived()
-            ->where('form_date', 'like', date('Y-m-d', strtotime($cut_off_account->formulir->created_at)) . '%')
+            ->where('form_date', 'like', date('Y-m-d', strtotime($cut_off_account->formulir->form_date)) . '%')
             ->selectOriginal()
             ->orderby('id', 'desc')
             ->first();
@@ -209,12 +209,12 @@ class CutOffHelper
                 if ($coa) {
                     $list_fixed_assets_detail = CutOffFixedAssetsDetail::where('fixed_assets_id', $cut_off_fixed_assets->id)->where('coa_id', $cut_off_account_detail->coa_id)->get();
                     foreach ($list_fixed_assets_detail as $fixed_assets_detail) {
-                        $journal_value = JournalHelper::getTotalValue($cut_off_account_detail->coa_id, $cut_off_account->formulir->created_at);
+                        $journal_value = JournalHelper::getTotalValue($cut_off_account_detail->coa_id, $cut_off_account->formulir->form_date);
                         $position = JournalHelper::position($cut_off_account_detail->coa_id);
                         $total = abs($journal_value->debit - $journal_value->credit);
                         if ($total > 0) {
                             $journal = new Journal();
-                            $journal->form_date = date('Y-m-d H:i:s', strtotime($cut_off_account->formulir->created_at));
+                            $journal->form_date = date('Y-m-d 23:59:59', strtotime($cut_off_account->formulir->form_date));
                             $journal->coa_id = $co_journal->coa_id;
                             $journal->description = "Cut Off from formulir number " . $cut_off_account->formulir->form_number;
                             $journal->$position = $total * -1;
@@ -235,7 +235,7 @@ class CutOffHelper
         $cut_off_inventory = CutOffInventory::joinFormulir()
             ->approvalApproved()
             ->open()
-            ->where('form_date', 'like', date('Y-m-d', strtotime($cut_off_account->formulir->created_at)) . '%')
+            ->where('form_date', 'like', date('Y-m-d', strtotime($cut_off_account->formulir->form_date)) . '%')
             ->selectOriginal()
             ->orderby('id', 'desc')
             ->first();
@@ -251,7 +251,7 @@ class CutOffHelper
 
                 // CUTOFF INVENTORY
                 $inventory = new Inventory;
-                $inventory->form_date = date('Y-m-d H:i:s', strtotime($cut_off_inventory->formulir->created_at));
+                $inventory->form_date = date('Y-m-d 23:59:59', strtotime($cut_off_inventory->formulir->form_date));
                 $inventory->formulir_id = $cut_off_inventory->formulir_id;
                 $inventory->warehouse_id = $cut_off_inventory_detail->warehouse_id;
                 $inventory->item_id = $cut_off_inventory_detail->subledger_id;
@@ -263,7 +263,7 @@ class CutOffHelper
 
                 // CUTOFF JOURNAL
                 $journal = new Journal();
-                $journal->form_date = date('Y-m-d H:i:s', strtotime($cut_off_account->formulir->created_at));
+                $journal->form_date = date('Y-m-d 23:59:59', strtotime($cut_off_account->formulir->form_date));
                 $journal->coa_id = $cut_off_account_detail->coa_id;
                 $journal->description = "Cut Off from formulir number ".$cut_off_account->formulir->form_number;
                 $journal->$position = $cut_off_inventory_detail->amount;
@@ -286,7 +286,7 @@ class CutOffHelper
             ->approvalApproved()
             ->open()
             ->notArchived()
-            ->where('form_date', 'like', date('Y-m-d', strtotime($cut_off_account->formulir->created_at)) . '%')
+            ->where('form_date', 'like', date('Y-m-d', strtotime($cut_off_account->formulir->form_date)) . '%')
             ->selectOriginal()
             ->orderby('id', 'desc')
             ->first();
@@ -294,7 +294,7 @@ class CutOffHelper
         if ($cut_off_payable) {
             foreach ($cut_off_payable->cutOffPayableDetail as $cut_off_payable_detail) {
                 $journal = new Journal();
-                $journal->form_date = date('Y-m-d H:i:s', strtotime($cut_off_account->formulir->created_at));
+                $journal->form_date = date('Y-m-d 23:59:59', strtotime($cut_off_account->formulir->form_date));
                 $journal->coa_id = $cut_off_account_detail->coa_id;
                 $journal->description = "Cut Off from formulir number ".$cut_off_account->formulir->form_number;
                 $journal->debit = 0;
@@ -318,7 +318,7 @@ class CutOffHelper
             ->approvalApproved()
             ->open()
             ->notArchived()
-            ->where('form_date', 'like', date('Y-m-d', strtotime($cut_off_account->formulir->created_at)) . '%')
+            ->where('form_date', 'like', date('Y-m-d', strtotime($cut_off_account->formulir->form_date)) . '%')
             ->selectOriginal()
             ->orderby('id', 'desc')
             ->first();
@@ -326,7 +326,7 @@ class CutOffHelper
         if ($cut_off_receivable) {
             foreach ($cut_off_receivable->cutOffReceivableDetail as $cut_off_receivable_detail) {
                 $journal = new Journal();
-                $journal->form_date = date('Y-m-d H:i:s', strtotime($cut_off_account->formulir->created_at));
+                $journal->form_date = date('Y-m-d 23:59:59', strtotime($cut_off_account->formulir->form_date));
                 $journal->coa_id = $cut_off_account_detail->coa_id;
                 $journal->description = "Cut Off from formulir number ".$cut_off_account->formulir->form_number;
                 $journal->debit = $cut_off_receivable_detail->amount;
@@ -350,7 +350,7 @@ class CutOffHelper
 //            ->approvalApproved()
 //            ->open()
 //            ->notArchived()
-//            ->where('form_date', 'like', date('Y-m-d', strtotime($cut_off_account->formulir->created_at)) . '%')
+//            ->where('form_date', 'like', date('Y-m-d', strtotime($cut_off_account->formulir->form_date)) . '%')
 //            ->selectOriginal()
 //            ->orderby('id', 'desc')
 //            ->get();
@@ -360,7 +360,7 @@ class CutOffHelper
 //            foreach ($cut_off_fixed_assets->cutOffFixedAssetsDetail->where('coa_id', $cut_off_account_detail->coa_id) as $cut_off_fixed_assets_detail) {
 //                $position = JournalHelper::position($cut_off_account_detail->coa_id);
 //                $journal = new Journal();
-//                $journal->form_date = date('Y-m-d H:i:s', strtotime($cut_off_account->formulir->created_at));
+//                $journal->form_date = date('Y-m-d 23:59:59', strtotime($cut_off_account->formulir->form_date));
 //                $journal->coa_id = $cut_off_account_detail->coa_id;
 //                $journal->description = "Cut Off from formulir number ".$cut_off_account->formulir->form_number;
 //                $journal->$position = $cut_off_fixed_assets_detail->total_price;
@@ -381,7 +381,7 @@ class CutOffHelper
     {
         if ($cut_off_account_detail->debit > 0 || $cut_off_account_detail->credit > 0) {
             $journal = new Journal();
-            $journal->form_date = date('Y-m-d H:i:s', strtotime($cut_off_account->formulir->created_at));
+            $journal->form_date = date('Y-m-d 23:59:59', strtotime($cut_off_account->formulir->form_date));
             $journal->coa_id = $cut_off_account_detail->coa_id;
             $journal->description = "Cut Off from formulir number " . $cut_off_account->formulir->form_number;
             $journal->debit = $cut_off_account_detail->debit;
@@ -472,7 +472,7 @@ class CutOffHelper
             get_class(new CutOffFixedAssets()),
         );
 
-        $formulir_relation = Formulir::where('form_date', $formulir->created_at)
+        $formulir_relation = Formulir::where('form_date', $formulir->form_date)
             ->whereIn('formulirable_type', $formulirable_type)
             ->whereNotNull('form_number')
             ->get();
