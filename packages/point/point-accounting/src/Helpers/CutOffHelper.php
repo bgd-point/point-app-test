0c642a4cd781bca3c9831de910bb707c5b292a5d
Martien Dermawan Tanama <martiendt@gmail.com>
2025-06-25 07:27:42 +0700

    x

--- packages/point/point-accounting/src/Helpers/CutOffHelper.php
+++ packages/point/point-accounting/src/Helpers/CutOffHelper.php
@@ -331,7 +331,7 @@ class CutOffHelper
             ->first();
 
         if ($cut_off_receivable) {
-            \Log::info('Account '.$cut_off_account_detail->coa->name . ' = ' . cut_off_account_detail->debit . ' / ' . cut_off_account_detail->credit);
+            \Log::info('Account ' . $cut_off_account_detail->coa->name . ' = ' . $cut_off_account_detail->debit . ' / ' . $cut_off_account_detail->credit);
             foreach ($cut_off_receivable->cutOffReceivableDetail as $cut_off_receivable_detail) {
                 \Log::info('Receivable ' . $cut_off_receivable_detail->coa->name . ' = ' . $cut_off_receivable_detail->amount);
                 if ($cut_off_account_detail->coa_id == $cut_off_receivable_detail->coa_id) {
