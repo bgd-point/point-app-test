916ffbe24206a6fb0e67fd0002b5845219a255c3
Martien Dermawan Tanama <martiendt@gmail.com>
2025-06-25 13:07:23 +0700

    x

--- packages/point/point-framework/src/Helpers/JournalHelper.php
+++ packages/point/point-framework/src/Helpers/JournalHelper.php
@@ -299,6 +299,7 @@ class JournalHelper
             ->get();
 
         if ($journal->debit != $journal->credit) {
+            dd($journal->debit != $journal->credit);
             dd($journals->toArray());
             throw new PointException('Journal unbalance, '. $journal->debit .' = '. $journal->credit .' Please contact administrator to fix this error');
         }
