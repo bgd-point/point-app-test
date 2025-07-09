536c953b29b3232a02d99a1acfd4f259be2ec48d
Martien Dermawan Tanama <martiendt@gmail.com>
2025-07-09 07:47:42 +0700

    x

--- app/Console/Commands/ReHppBBL.php
+++ app/Console/Commands/ReHppBBL.php
@@ -4,9 +4,12 @@ namespace App\Console\Commands;
 
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use Point\Framework\Helpers\InventoryHelper;
-use Point\Framework\Models\Formulir;
+use Point\Framework\Helpers\JournalHelper;
 use Point\Framework\Models\Inventory;
+use Point\Framework\Models\Journal;
+use Point\Framework\Models\Formulir;
 use Point\Framework\Models\Master\Allocation;
 use Point\PointInventory\Models\StockOpname\StockOpname;
 use Point\PointInventory\Models\StockOpname\StockOpnameItem;
@@ -35,7 +38,7 @@ class ReHppBBL extends Command
      */
     public function handle()
     {
-        $this->comment('recalculating inventory');
+        $this->comment('recalculating hpp');
 
         \DB::beginTransaction();
 
@@ -43,40 +46,48 @@ class ReHppBBL extends Command
         $inventories = Inventory::orderBy('form_date', 'asc')
             ->get()
             ->unique(function ($inventory) {
-                return $inventory['item_id'].$inventory['warehouse_id'];
+                return $inventory['item_id'];
             });
 
         foreach ($inventories as $inventory) {
 
+            $totalQ = Inventory::with('formulir')
+                ->where('item_id', '=', $inventory->item_id)
+		        ->where('form_date', '<', '2025-06-01')
+                ->sum('quantity');
+
+            $journal = Journal::where('subledger_type', 'Point\Framework\Models\Master\Item')
+                ->where('subledger_id', $inventory->item_id)
+                ->where('form_date', '<', '2025-06-01')
+                ->selectRaw('sum(debit) as debit, sum(credit) as credit, count(coa_id) as counter')
+                ->first();
+
+            $totalV = $journal->debit - $journal->credit;
+
             $list_inventory = Inventory::with('formulir')
                 ->where('item_id', '=', $inventory->item_id)
-                ->where('warehouse_id', '=', $inventory->warehouse_id)
-		        ->where('form_date', '>=', '2024-01-01')
+		        ->where('form_date', '<', '2025-06-01')
                 ->orderBy('form_date', 'asc')
-                ->orderBy('quantity', 'desc')
                 ->get();
+
+            if ($totalV < 0 ||  $totalQ < 0) {
+                $this->comment('problem item ' . $inventory->item_id);
+                continue;
+            }
+
+            $hpp = $totalV / $totalQ
 
             $totalQty = 0;
             $totalValue = 0;
             foreach($list_inventory as $index => $l_inventory) {
-                if ($index === 0) {
-                    $totalQty = $l_inventory->total_quantity;
-                    $totalValue = $l_inventory->quantity * $l_inventory->price;
-                    $l_inventory->recalculate = 0;
-                    $l_inventory->total_value = $totalValue;
-                    $l_inventory->save();
-                } else if ($l_inventory->formulir->formulirable_type === StockOpname::class) {
-                    $l_inventory->recalculate = 0;
-                    $l_inventory->save();
-                    $totalQty = $l_inventory->total_quantity;
-                } else {
-                    $l_inventory->recalculate = 0;
-                    $l_inventory->total_quantity = $totalQty + $l_inventory->quantity;
-                    $l_inventory->total_value = $totalValue + ($l_inventory->quantity * $l_inventory->price);
-                    $l_inventory->save();
-                    $totalQty = $l_inventory->total_quantity;
-                    $totalValue = $l_inventory->total_value;
-                }
+
+                $value = abs($l_inventory->quantity) * $hpp;
+                $totalValue = $totalValue + $value;
+
+                $l_inventory->recalculate = 0;
+                $l_inventory->cogs = $hpp;
+                $l_inventory->total_value = $totalValue;
+                $l_inventory->save();
             }
         }
 
