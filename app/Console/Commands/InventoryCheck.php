de862ecb20b262019a4d22529dd70a9b81a64455
Martien Dermawan Tanama <martiendt@gmail.com>
2025-09-11 16:44:54 +0700

    x

--- app/Console/Commands/InventoryCheck.php
+++ app/Console/Commands/InventoryCheck.php
@@ -7,6 +7,7 @@ use Point\Framework\Models\Inventory;
 use Point\Framework\Models\Journal;
 use Point\Framework\Models\Master\Item;
 use Point\PointAccounting\Models\MemoJournal;
+use Point\PointInventory\Models\TransferItem\TransferItem;
 
 class InventoryCheck extends Command
 {
@@ -41,49 +42,21 @@ class InventoryCheck extends Command
      */
     public function handle()
     {
-        $memoJournals = MemoJournal::all();
-        foreach ($memoJournals as $memoJournal) {
-            foreach ($memoJournal->detail as $detail) {
-                if ($detail->coa->category->name === 'Inventories' || $detail->coa->name === 'BEBAN POKOK PENJUALAN') {
-                    Journal::where('form_journal_id', $memoJournal->formulir_id)->where('coa_id', $detail->coa_id)->delete();
-                    $detail->delete();
-                }
+        $this->line('STARTING');
+
+        $transferItems = TransferItem::join('formulir', 'formulir.id', '=', 'point_inventory_transfer_item')
+            ->where('formulir.approval_status', 1)
+            ->where('formulir.form_status', 1)
+            ->select('point_inventory_transfer_item.*')
+            ->get();
+
+        foreach ($transferItems as $transferItem) {
+            $inv = Inventory::where('formulir_id', '=', $transferItem->formulir_id)->sum('quantity');
+            if ($inv > 0) {
+                $this->line($transferItem->formulir_id . ' - Problem TI');
             }
         }
 
-        $journals = Journal::where('coa_id', 8)->get();
-        foreach ($journals as $journal) {
-            $inventory = Inventory::where('formulir_id', $journal->form_journal_id)->get();
-
-            if ($inventory->count() == 0) {
-                $this->line('JOURNAL: ' . $journal->formulir->form_number . ', ID: ' . $journal->id . ', TOTAL: ' . ($journal->debit + $journal->credit));
-                $journal->delete();
-            }
-        }
-
-        $inventories = Inventory::all();
-        $sumI = 0;
-        foreach ($inventories as $inventory) {
-            $journal = Journal::where('form_journal_id', $inventory->formulir_id)->get();
-
-            if ($journal->count() == 0) {
-                $this->line('INVENTORY: ' . $inventory->formulir->form_number . ', ID: ' . $inventory->id . ', TOTAL: ' . $inventory->total_value);
-                $sumI += $inventory->total_value;
-
-                $this->line('Fixing ' . $inventory->id);
-                $journal = new Journal;
-                $journal->coa_id = 8;
-                $journal->debit = $inventory->total_value;
-                $journal->form_date = $inventory->form_date;
-                $journal->description = 'Cut Off';
-                $journal->form_journal_id = 111;
-                $journal->form_reference_id;
-                $journal->subledger_id = $inventory->item_id;
-                $journal->subledger_type = get_class(new Item());
-                $journal->save();
-            }
-        }
-
-        $this->line('SUM INVENTORY: ' . $sumI);
+        $this->line('ENDING');
     }
 }
