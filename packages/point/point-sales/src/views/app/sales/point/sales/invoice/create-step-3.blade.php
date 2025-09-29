db265ae43118e8ee663784390e6062fd2d65feb1
Martien Dermawan Tanama <martiendt@gmail.com>
2025-09-29 15:38:13 +0700

    x

--- packages/point/point-sales/src/views/app/sales/point/sales/invoice/create-step-3.blade.php
+++ packages/point/point-sales/src/views/app/sales/point/sales/invoice/create-step-3.blade.php
@@ -29,7 +29,7 @@
                         <label class="col-md-3 control-label">Date</label>
 
                         <div class="col-md-3">
-                            <input type="text" name="form_date" class="form-control date input-datepicker"
+                            <input readonly type="text" name="form_date" class="form-control date input-datepicker"
                                    data-date-format="{{date_format_masking()}}" placeholder="{{date_format_masking()}}"
                                    value="{{ date(date_format_get(), strtotime(\Carbon::now())) }}">
                         </div>
