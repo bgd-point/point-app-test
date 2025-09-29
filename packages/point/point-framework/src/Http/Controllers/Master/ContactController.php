0a775bc6c879dbaed5ff7e0c15b58272e6e06f46
Martien Dermawan Tanama <martiendt@gmail.com>
2025-09-29 07:06:07 +0700

    x

--- packages/point/point-framework/src/Http/Controllers/Master/ContactController.php
+++ packages/point/point-framework/src/Http/Controllers/Master/ContactController.php
@@ -36,7 +36,7 @@ class ContactController extends Controller
         $view->person_type = $person_type;
         \Log::info('person: ' . $person_type_slug . ' - ' . $person_type);
         $view->list_contact = Person::searchByType($person_type->id, $request->input('status'),$request->input('search'))->search($request->input('search'))->paginate(100);
-        \Log::info('result person: ' . $$view->list_contact);
+        \Log::info('result person: ' . $view->list_contact);
 
         return $view;
     }
