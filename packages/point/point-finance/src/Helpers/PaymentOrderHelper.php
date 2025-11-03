d85679cf02abe875ee9bbc69d552f9188a33121d
Martien Dermawan Tanama <martiendt@gmail.com>
2025-11-04 06:27:14 +0700

    x

--- packages/point/point-finance/src/Helpers/PaymentOrderHelper.php
+++ packages/point/point-finance/src/Helpers/PaymentOrderHelper.php
@@ -41,7 +41,7 @@ class PaymentOrderHelper
             });
         }
 
-        if (request()->get('database_name') == 'p_test' && request()->get('database_name') == 'p_personalfinance' && auth()->user()->name != 'lioni') {
+        if ((request()->get('database_name') == 'p_test' || request()->get('database_name') == 'p_personalfinance') && auth()->user()->name != 'lioni') {
             $list_payment_order = $list_payment_order->join('point_finance_payment_order_detail', 'point_finance_payment_order.id', '=', 'point_finance_payment_order_detail.point_finance_payment_order_id')
             ->join('coa', 'coa.id', '=', 'point_finance_payment_order_detail.coa_id')
             ->where('coa.name', 'not like', '%HONORARIUM%')
