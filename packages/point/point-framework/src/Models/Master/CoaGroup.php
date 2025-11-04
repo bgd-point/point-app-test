2b3cb892cacbf3c7829da23d3347a65db93dcf4e
Martien Dermawan Tanama <martiendt@gmail.com>
2025-11-04 09:17:52 +0700

    x

--- packages/point/point-framework/src/Models/Master/CoaGroup.php
+++ packages/point/point-framework/src/Models/Master/CoaGroup.php
@@ -44,7 +44,6 @@ class CoaGroup extends Model
     public function coa()
     {
         if ((request()->get('database_name') == 'p_test' || request()->get('database_name') == 'p_personalfinance') && auth()->user()->name != 'lioni') {
-            dd('g');
             return $this->hasMany('Point\Framework\Models\Master\Coa', 'coa_group_id')
                 ->where('coa.name', 'not like', '%lioni%')
                 ->orderBy('coa_number', 'asc');
