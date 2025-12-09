d5578a1a6c385e1675233b3f61b641df07678caa
Martien Dermawan Tanama <martiendt@gmail.com>
2025-12-09 08:56:46 +0700

    x

--- config/app.php
+++ config/app.php
@@ -143,7 +143,7 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
+        // App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
