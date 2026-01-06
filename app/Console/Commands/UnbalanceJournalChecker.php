985b2ae436e46b9276665e4aa5fe5911f0804e6c
Martien Dermawan Tanama <martiendt@gmail.com>
2026-01-06 13:47:04 +0700

    x

--- app/Console/Commands/UnbalanceJournalChecker.php
+++ app/Console/Commands/UnbalanceJournalChecker.php
@@ -37,5 +37,23 @@ class UnbalanceJournalChecker extends Command
                 $this->comment($journal->form_journal_id . ' ' . $journal->formulir->form_number . ' ' . $journal->formulir->formulirable_type . ' ' . $debit . ' ' . $credit);
             }
         }
+
+        $this->comment('second attempt');
+
+        $journals = Journal::select(
+            'form_journal_id',
+            DB::raw('SUM(debit) as total_debit'),
+            DB::raw('SUM(credit) as total_credit')
+        )
+        ->where('form_date', '>=', '2025-11-01')
+        ->groupBy('form_journal_id')
+        ->havingRaw('ABS(SUM(debit) - SUM(credit)) > 0.01')
+        ->get();
+
+        foreach ($journals as $journal) {
+            $this->comment(
+                "{$journal->form_journal_id} | Debit: {$journal->total_debit} | Credit: {$journal->total_credit}"
+            );
+        }
     }
 }
