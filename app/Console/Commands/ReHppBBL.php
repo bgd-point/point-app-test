f450752b81c585a893a94f9c2e4422e5008a97fe
Martien Dermawan Tanama <martiendt@gmail.com>
2026-01-09 05:54:43 +0700

    x

--- app/Console/Commands/ReHppBBL.php
+++ app/Console/Commands/ReHppBBL.php
@@ -64,11 +64,11 @@ class ReHppBBL extends Command
                 $value = $l_inventory->quantity * $l_inventory->price;
                 $totalValue = round($totalValue + $value, 4);
 
-                $l_inventory->cogs = $totalValue / $l_inventory->total_quantity;
+                $this->comment($l_inventory->form->form_number . ' ' . $totalValue . ' / ' . $l_inventory->total_quantity);
+                // $l_inventory->cogs = $totalValue / $l_inventory->total_quantity;
                 $l_inventory->recalculate = 0;
                 $l_inventory->total_value = $totalValue;
                 $l_inventory->save();
-                $this->comment($l_inventory->form_date);
             }
         }
 
