190622dbf3fd2247d5cde914ba8031331247571b
Martien Dermawan Tanama <martiendt@gmail.com>
2026-04-13 14:48:46 +0700

    x

--- app/Console/Commands/RecalculateBBL.php
+++ app/Console/Commands/RecalculateBBL.php
@@ -47,7 +47,7 @@ class RecalculateBBL extends Command
             ->delete();
         
         $inventories = Inventory::orderBy('form_date', 'asc')
-            ->where('item_id', '=', $inventory->item_id)
+            ->where('item_id', '=', 606)
             ->get()
             ->unique(function ($inventory) {
                 return $inventory['item_id'];
