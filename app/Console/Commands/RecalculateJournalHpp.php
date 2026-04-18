86a16e9d9ffdf6f5a6bdb04658d205a10951ced8
Martien Dermawan Tanama <martiendt@gmail.com>
2026-04-18 09:23:25 +0700

    x

--- app/Console/Commands/RecalculateJournalHpp.php
+++ app/Console/Commands/RecalculateJournalHpp.php
@@ -61,6 +61,7 @@ class RecalculateJournalHpp extends Command
          * FIX INVENTORY COGS != JOURNAL
          */
 
+        // INVOICE
         $inventories = Inventory::join('formulir', 'formulir.id', '=', 'inventory.formulir_id')
             ->where('formulir.formulirable_type', '=', 'Point\PointSales\Models\Sales\Invoice')
             ->select('inventory.*')
@@ -106,7 +107,6 @@ class RecalculateJournalHpp extends Command
                 $jHpp->credit = $iValue;
             }
             $jHpp->save();
-
         }
 
         // TI
@@ -140,64 +140,95 @@ class RecalculateJournalHpp extends Command
             }
         }
 
+        // INPUT MANUFACTURE
+        $inventories = Inventory::join('formulir', 'formulir.id', '=', 'inventory.formulir_id')
+            ->where('formulir.formulirable_type', '=', 'Point\PointManufacture\Models\InputProcess')
+            ->select('inventory.*')
+            ->get();
+
+        foreach($inventories as $inventory) {
+            $journals = Journal::where('form_journal_id', '=', $inventory->formulir_id)
+                ->where('journal.subledger_id', '=', $inventory->item_id)
+                ->where('journal.subledger_type', '=', "Point\Framework\Models\Master\Item")
+                ->select('journal.*')
+                ->get();
+
+            if (!count($journals)) {
+                $this->comment('Journal not found | inventory_id: ' . $inventory->id . ' | formulir_id: ' . $inventory->formulir_id);
+                continue;
+            }
+
+            $iValue = round(abs($inventory->quantity * $inventory->price), 4);
+
+            foreach ($journals as $journal) {
+                $this->comment($journal->description);
+                if ($journal->debit > 0) {
+                    $journal->debit = $iValue;
+                } else {
+                    $journal->credit = $iValue;
+                }
+                $journal->save();
+            }
+        }
+
         /**
          * FIX OUTPUT SELISIH KOMA
          */
 
-        $journals = Journal::join('coa', 'coa.id', '=', 'journal.coa_id')
-            ->join('formulir', 'formulir.id', '=', 'journal.form_journal_id')
-            ->where('formulir.formulirable_type', '=', 'Point\PointManufacture\Models\OutputProcess')
-            ->where('journal.debit', '>', 0)
-            ->select('journal.*')
-            ->get();
+        // $journals = Journal::join('coa', 'coa.id', '=', 'journal.coa_id')
+        //     ->join('formulir', 'formulir.id', '=', 'journal.form_journal_id')
+        //     ->where('formulir.formulirable_type', '=', 'Point\PointManufacture\Models\OutputProcess')
+        //     ->where('journal.debit', '>', 0)
+        //     ->select('journal.*')
+        //     ->get();
 
-        foreach($journals as $journal) {
-            $inventory = Inventory::where('formulir_id', '=', $journal->form_journal_id)
-                ->where('item_id', '=', $journal->subledger_id)
-                ->first();
+        // foreach($journals as $journal) {
+        //     $inventory = Inventory::where('formulir_id', '=', $journal->form_journal_id)
+        //         ->where('item_id', '=', $journal->subledger_id)
+        //         ->first();
 
-            $a = $inventory->price * $inventory->quantity;
-            $b = $journal->debit;
+        //     $a = $inventory->price * $inventory->quantity;
+        //     $b = $journal->debit;
                 
-            if ($a !== $b) {
-                $c = $b - $a;
-                $this->comment($journal->id . ' & ' . $journal->form_journal_id . ' = ' . $a . ' = ' . $b . ' = ' . ($b - $a));
+        //     if ($a !== $b) {
+        //         $c = $b - $a;
+        //         $this->comment($journal->id . ' & ' . $journal->form_journal_id . ' = ' . $a . ' = ' . $b . ' = ' . ($b - $a));
     
-                $j = new Journal();
-                $j->form_date = $journal->form_date;
-                $j->coa_id = $journal->coa_id;
-                $j->description = 'Pembulatan';
-                if ($c > 0) {
-                    $j->debit = 0;
-                    $j->credit = abs($c);
-                } else {
-                    $j->debit = abs($c);
-                    $j->credit = 0;
-                }
-                $j->form_journal_id = $journal->form_journal_id;
-                $j->form_reference_id = $journal->form_reference_id;
-                $j->subledger_id = $journal->subledger_id;
-                $j->subledger_type = $journal->subledger_type;
-                $j->save();
+        //         $j = new Journal();
+        //         $j->form_date = $journal->form_date;
+        //         $j->coa_id = $journal->coa_id;
+        //         $j->description = 'Pembulatan';
+        //         if ($c > 0) {
+        //             $j->debit = 0;
+        //             $j->credit = abs($c);
+        //         } else {
+        //             $j->debit = abs($c);
+        //             $j->credit = 0;
+        //         }
+        //         $j->form_journal_id = $journal->form_journal_id;
+        //         $j->form_reference_id = $journal->form_reference_id;
+        //         $j->subledger_id = $journal->subledger_id;
+        //         $j->subledger_type = $journal->subledger_type;
+        //         $j->save();
                 
-                $j = new Journal();
-                $j->form_date = $journal->form_date;
-                $j->coa_id = 472;
-                $j->description = 'Pembulatan';
-                if ($c > 0) {
-                    $j->debit = abs($c);
-                    $j->credit = 0;
-                } else {
-                    $j->debit = 0;
-                    $j->credit = abs($c);
-                }
-                $j->form_journal_id = $journal->form_journal_id;
-                $j->form_reference_id = $journal->form_reference_id;
-                $j->subledger_id = $journal->subledger_id;
-                $j->subledger_type = $journal->subledger_type;
-                $j->save();
-            }
-        }
+        //         $j = new Journal();
+        //         $j->form_date = $journal->form_date;
+        //         $j->coa_id = 472;
+        //         $j->description = 'Pembulatan';
+        //         if ($c > 0) {
+        //             $j->debit = abs($c);
+        //             $j->credit = 0;
+        //         } else {
+        //             $j->debit = 0;
+        //             $j->credit = abs($c);
+        //         }
+        //         $j->form_journal_id = $journal->form_journal_id;
+        //         $j->form_reference_id = $journal->form_reference_id;
+        //         $j->subledger_id = $journal->subledger_id;
+        //         $j->subledger_type = $journal->subledger_type;
+        //         $j->save();
+        //     }
+        // }
 
         \DB::commit(); 
     }
