59a6d4d7cf3c6c513a05a9465442d3fa201bf282
Martien Dermawan Tanama <martiendt@gmail.com>
2026-04-28 12:08:15 +0700

    x

--- app/Console/Commands/RecalculateTransaction.php
+++ app/Console/Commands/RecalculateTransaction.php
@@ -54,6 +54,109 @@ class RecalculateTransaction extends Command
             $this->comment($invoice->formulir_id);
             Inventory::where('formulir_id', '=', $invoice->formulir->id)->delete();
             Journal::where('form_journal_id', '=', $invoice->formulir->id)->delete();
+
+            foreach ($invoice->items as $invoice_detail) {
+                $goods_received_item = ReferHelper::getReferBy(get_class($invoice_detail), $invoice_detail->id, get_class($invoice), $invoice->id);
+                if ($goods_received_item) {
+                    $goods_received = GoodsReceived::find($goods_received_item->point_purchasing_goods_received_id);
+                    $warehouse_id = $goods_received->warehouse_id;
+                }
+    
+                // Journal inventory
+                $total_per_row = $invoice_detail->quantity * $invoice_detail->price - $invoice_detail->quantity * $invoice_detail->price / 100 * $invoice_detail->discount;
+                if ($invoice->discount) {
+                    $discounty = $total_per_row * $discount / $subtotal;
+                    $total_per_row = $total_per_row - $discounty;
+                }
+                if ($request->input('type_of_tax') == 'include') {
+                    $total_per_row = $total_per_row * 100 / 111;
+                }
+
+                $position = JournalHelper::position($invoice_detail->item->account_asset_id);
+
+                $journal = new Journal();
+                $journal->form_date = date('Y-m-d H:i:s');
+                $journal->coa_id = $invoice_detail->item->account_asset_id;
+                $journal->description = 'Goods Received [' . $invoice->formulir->form_number.']';
+                $journal->$position = $total_per_row;
+                $journal->form_journal_id = $invoice->formulir_id;
+                $journal->form_reference_id;
+                $journal->subledger_id = $invoice_detail->item_id;
+                $journal->subledger_type = get_class($invoice_detail->item);
+                $journal->save();
+
+                $dc->debit += round($total_per_row, 4);
+
+                // insert new inventory
+                $item = Item::find($invoice_detail->item_id);
+                $inventory = new Inventory();
+                $inventory->formulir_id = $formulir->id;
+                $inventory->item_id = $item->id;
+                $inventory->quantity = $invoice_detail->quantity * $invoice_detail->converter;
+
+                $price = $total_per_row / $inventory->quantity;
+                if ($request->input('type_of_tax') == 'include') {
+                    $inventory->price = ($price * 100 / 111) / $invoice_detail->converter;
+                } else {
+                    $inventory->price = $price / $invoice_detail->converter;
+                }
+                $inventory->form_date = date('Y-m-d H:i:s');
+                $inventory->warehouse_id = $warehouse_id;
+
+                $inventory_helper = new InventoryHelper($inventory);
+                $inventory_helper->in();
+            }
+
+            // Journal tax exclude and non-tax
+            if ($request->input('type_of_tax') == 'exclude' || $request->input('type_of_tax') == 'non') {
+                $data = array(
+                    'value_of_account_payable' => $total,
+                    'value_of_income_tax_receiveable' => $tax,
+                    'value_of_expedition_cost' => $invoice->expedition_fee,
+                    'formulir' => $formulir,
+                    'invoice' => $invoice
+                );
+                $dc2 = self::journal($data);
+                $dc->debit += round($dc2->debit , 4);
+                $dc->credit += round($dc2->credit , 4);
+            } elseif ($request->input('type_of_tax') == 'include') {
+                $data = array(
+                    'value_of_account_payable' => $total,
+                    'value_of_income_tax_receiveable' => $tax,
+                    'value_of_expedition_cost' => $invoice->expedition_fee,
+                    'formulir' => $formulir,
+                    'invoice' => $invoice
+                );
+                $dc2 = self::journal($data);
+                $dc->debit += round($dc2->debit, 4);
+                $dc->credit += round($dc2->credit, 4);
+            }
+
+            
+            if($dc->debit !== $dc->credit) {
+                
+                $factor = pow(10, 4);
+                
+                $fdebit = floor($dc->debit * $factor) / $factor;
+                $fcredit = floor($dc->credit * $factor) / $factor;
+
+                $journal = new Journal();
+                $journal->form_date = date('Y-m-d H:i:s');
+                $coa_selisih = \DB::table('coa')->where('name', 'PENDAPATAN (BEBAN) SELISIH PEMBAYARAN')->first();
+                if ($coa_selisih) {
+                    $journal->coa_id = $coa_selisih->id;
+                } else {
+                    $journal->coa_id = 1193;
+                }
+                $journal->description = 'Selisih pembulatan';
+                $journal->debit = JournalHelper::getDiff($invoice->formulir_id);
+                $journal->credit = 0;
+                $journal->form_journal_id = $invoice->formulir_id;
+                $journal->form_reference_id;
+                $journal->subledger_id;
+                $journal->subledger_type;
+                $journal->save();
+            }
         }
 
         $this->comment('recalculating transaction sales');
@@ -135,6 +238,65 @@ class RecalculateTransaction extends Command
         }
 
         \DB::commit();
+    }
+
+    public static function journalPurchasing($data)
+    {
+        $dc = new \stdClass();
+        $dc->debit = 0;
+        $dc->credit = 0;
+
+        // 1. Journal account receiveable
+        $account_receiveable = JournalHelper::getAccount('point purchasing', 'account payable');
+        $position = JournalHelper::position($account_receiveable);
+        
+        $journal = new Journal;
+        $journal->form_date = date('Y-m-d H:i:s');
+        $journal->coa_id = $account_receiveable;
+        $journal->description = 'Invoice Purchasing [' . $data['formulir']->form_number.']';
+        $journal->$position = $data['value_of_account_payable'];
+        $journal->form_journal_id = $data['formulir']->id;
+        $journal->form_reference_id;
+        $journal->subledger_id = $data['invoice']->supplier_id;
+        $journal->subledger_type = get_class($data['invoice']->supplier);
+        $journal->save();
+        
+        $dc->credit += round($data['value_of_account_payable'], 4);
+
+        // 2. Journal income tax receiveable
+        $income_tax_receiveable = JournalHelper::getAccount('point purchasing', 'income tax receivable');
+        $position = JournalHelper::position($income_tax_receiveable);
+        $journal = new Journal;
+        $journal->form_date = date('Y-m-d H:i:s');
+        $journal->coa_id = $income_tax_receiveable;
+        $journal->description = 'Invoice Purchasing [' . $data['formulir']->form_number.']';
+        $journal->$position = $data['value_of_income_tax_receiveable'];
+        $journal->form_journal_id = $data['formulir']->id;
+        $journal->form_reference_id;
+        $journal->subledger_id;
+        $journal->subledger_type;
+        $journal->save();
+        
+        $dc->debit += round($data['value_of_income_tax_receiveable'], 4);
+
+        // 3. Journal Expedition Cost
+        if ($data['invoice']->expedition_fee > 0) {
+            $expedition = JournalHelper::getAccount('point purchasing', 'expedition cost');
+            $position = JournalHelper::position($expedition);
+            $journal = new Journal;
+            $journal->form_date = date('Y-m-d H:i:s');
+            $journal->coa_id = $expedition;
+            $journal->description = 'Invoice Purchasing [' . $data['formulir']->form_number.']';
+            $journal->$position = $data['value_of_expedition_cost'];
+            $journal->form_journal_id = $data['formulir']->id;
+            $journal->form_reference_id;
+            $journal->subledger_id;
+            $journal->subledger_type;
+            $journal->save();
+
+            $dc->debit += round($data['value_of_expedition_cost'], 4);
+        }
+        return $dc;
     }
 
     public static function journalSales($data)
