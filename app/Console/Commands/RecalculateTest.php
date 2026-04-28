b0ca5e361e79f0d306f609fb7c5ae729160f4be5
Martien Dermawan Tanama <martiendt@gmail.com>
2026-04-28 15:46:14 +0700

    x

--- app/Console/Commands/RecalculateTest.php
+++ app/Console/Commands/RecalculateTest.php
@@ -36,10 +36,7 @@ class RecalculateTest extends Command
      */
     public function handle()
     {
-        Journal::where('description', 'Pembulatan')
-            ->whereNotNull('subledger_type')
-            ->delete();
-        self::fixInput();
+        self::fixOutput();
     }
 
     public static function fixInput()
@@ -49,6 +46,7 @@ class RecalculateTest extends Command
         foreach ($inputs as $input) {
 
         }
+
         // foreach ($input_approval->material as $material) {
         //     $inventory = new Inventory;
         //     $inventory->formulir_id = $input_approval->formulir->id;
@@ -66,76 +64,47 @@ class RecalculateTest extends Command
         // }
     }
 
-    public static function createOutput($request, $formulir)
+    public static function fixOutput()
     {
         $input_process = InputProcess::find($request->input('input_id'));
 
-        $output = new OutputProcess;
-        $output->formulir_id = $formulir->id;
-        $output->machine_id = $input_process->machine_id;
-        $output->input_id = $input_process->id;
-        $output->save();
+        $outputs = OutputProcess::join('formulir', 'point_manufacture_output.formulir_id', '=', 'formulir.id')
+            ->where('formulir.approval_status', 1)
+            ->where('formulir.form_status', 1)
+            ->get();
 
-        // close this form, doesn't need approval too
-        $output->formulir->form_status = 1;
-        $output->formulir->approval_status = 1;
-        $output->formulir->save();
+        foreach ($outputs as $output) {
+            Inventory::where('formulir_id', $output.formulir_id)->delete();
+            Journal::where('form_journal_id', $output.formulir_id)->delete();
 
-        $i = 0;
-        foreach ($input_process->product as $input_product) {
-            $quantity = number_format_db($request->input('quantity_output')[$i]);
-            $output_product = new OutputProduct;
-            $output_product->output_id = $output->id;
-            $output_product->warehouse_id = $input_product->warehouse_id;
-            $output_product->product_id = $input_product->product_id;
-            $output_product->quantity = $quantity;
-            $output_product->unit = $input_product->unit;
-            $output_product->converter = $input_product->converter;
-            $output_product->save();
-            $i++;
+            $value = InputProcess::join('formulir', 'point_manufacture_input.formulir_id', '=', 'formulir.id')
+                ->where('point_manufacture_input.id', $output->input_id)
+                ->sum('debit');
+
+            foreach ($output->products as $product) {
+                $totalQty = 0;
+                foreach ($output->product as $product) {
+                    $totalQty += (float) $product->quantity;
+                }
+
+                $cogs_product = $value / $totalQty;
+
+                foreach ($output->product as $product) {
+                    $inventory = new Inventory();
+                    $inventory->formulir_id = $product->formulir->id;
+                    $inventory->form_date = $product->formulir->approval_at;
+                    $inventory->warehouse_id = $product->warehouse_id;
+                    $inventory->item_id = $product->product_id;
+                    $inventory->quantity = $product->quantity;
+                    $inventory->price = $cogs_product;
+
+                    $inventory_helper = new InventoryHelper($inventory);
+                    $inventory_helper->in();
+
+                    self::addJournalOutput($inventory, $totalQty);
+                }
+            }
         }
-
-        // SET COGS FOR PRODUCT
-        $total_product_quantity = 0;
-        for ($i=0; $i < count($request->input('quantity_output')); $i++) {
-            $total_product_quantity += number_format_db($request->input('quantity_output')[$i]);
-        }
-
-        $total_value = Journal::where('form_journal_id', $input_process->formulir_id)->sum('debit');
-        $cogs_product = $total_value / $total_product_quantity;
-        
-        $i = 0;
-        $totalInv = 0;
-        foreach ($input_process->product as $input_product) {
-            $quantity = number_format_db($request->input('quantity_output')[$i]);
-            $totalInv += (float) $quantity;
-            $i++;
-        }
-
-        $i = 0;
-        foreach ($input_process->product as $input_product) {
-            $quantity = number_format_db($request->input('quantity_output')[$i]);
-            $inventory = new Inventory();
-            $inventory->formulir_id = $formulir->id;
-            $inventory->form_date = date('Y-m-d H:i:s');
-            $inventory->warehouse_id = $input_product->warehouse_id;
-            $inventory->item_id = $input_product->product_id;
-            $inventory->quantity = $quantity;
-            $inventory->price = $cogs_product;
-
-            $inventory_helper = new InventoryHelper($inventory);
-            $inventory_helper->in();
-
-            self::addJournalOutput($inventory, $totalInv);
-            $i++;
-        }
-
-        // update formulir input
-        formulir_lock($input_process->formulir_id, $formulir->id);
-        $input_process->formulir->form_status = 1;
-        $input_process->save();
-
-        return $output;
     }
 
     public static function addJournalInput($inventory)
