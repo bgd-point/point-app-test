f9e5dc395eceb9ce9021c989ad9af60080422647
Martien Dermawan Tanama <martiendt@gmail.com>
2026-04-28 17:04:21 +0700

    x

---
+++ app/Console/Commands/RecalculateInput.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Point\Framework\Helpers\InventoryHelper;
+use Point\Framework\Helpers\JournalHelper;
+use Point\Framework\Models\Formulir;
+use Point\Framework\Models\Inventory;
+use Point\Framework\Models\Master\Allocation;
+use Point\Framework\Models\Journal;
+use Point\Framework\Models\Master\Item;
+use Point\PointManufacture\Models\Formula;
+use Point\PointManufacture\Models\FormulaMaterial;
+use Point\PointManufacture\Models\FormulaProduct;
+use Point\PointManufacture\Models\InputMaterial;
+use Point\PointManufacture\Models\InputProcess;
+use Point\PointManufacture\Models\InputProduct;
+use Point\PointManufacture\Models\OutputProcess;
+use Point\PointManufacture\Models\OutputProduct;
+
+
+class RecalculateInput extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dev:recalculate:input';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'recalculate';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $inputs = InputProcess::join('formulir', 'point_manufacture_input.formulir_id', '=', 'formulir.id')->get();
+
+        foreach ($inputs as $input) {
+            foreach ($input->material as $material) {
+                \Log::info($input->id . ' = ' . $input->material);
+                $inventory = new Inventory;
+                $inventory->formulir_id = $input->formulir->id;
+                $inventory->item_id = $material->material_id;
+                $inventory->quantity = $material->quantity * InputMaterial::unit($material->material_id);
+                $inventory->cogs = InventoryHelper::getCostOfSales($input->formulir->approval_at, $material->material_id, $material->warehouse_id);
+                $inventory->price = $inventory->cogs;
+                $inventory->form_date = $input->formulir->approval_at;
+                $inventory->warehouse_id = $material->warehouse_id;
+
+                $inventory_helper = new InventoryHelper($inventory);
+                $inventory_helper->out();
+
+                self::addJournalInput($inventory);
+            }
+        }
+    }
+
+    public static function addJournalInput($inventory)
+    {
+        // JOURNAL #1 of #2
+        $journal = new Journal();
+        $journal->form_date = $inventory->formulir->approval_at;
+        $journal->coa_id = $inventory->item->account_asset_id;
+        $journal->description = 'Manufacture input process ' . $inventory->item->codeName;
+        $journal->debit = 0;
+        $journal->credit = abs($inventory->quantity * $inventory->price);
+        $journal->form_journal_id = $inventory->formulir->id;
+        $journal->form_reference_id;
+        $journal->subledger_id = $inventory->item_id;
+        $journal->subledger_type = get_class($inventory->item);
+        $journal->save();
+
+        // JOURNAL #2 of #2
+        $work_in_process_account_id = JournalHelper::getAccount('manufacture process', 'work in process');
+
+        $journal = new Journal();
+        $journal->form_date = $inventory->formulir->approval_at;
+        $journal->coa_id = $work_in_process_account_id;
+        $journal->description = 'Manufacture input process ' . $inventory->item->codeName;
+        $journal->debit = abs($inventory->quantity * $inventory->price);
+        $journal->credit = 0;
+        $journal->form_journal_id = $inventory->formulir->id;
+        $journal->form_reference_id;
+        $journal->subledger_id = $inventory->item_id;
+        $journal->subledger_type = get_class($inventory->item);
+        $journal->save();
+    }
+}
---
+++ app/Console/Commands/RecalculateOutput.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+use Point\Framework\Helpers\InventoryHelper;
+use Point\Framework\Helpers\JournalHelper;
+use Point\Framework\Models\Formulir;
+use Point\Framework\Models\Inventory;
+use Point\Framework\Models\Master\Allocation;
+use Point\Framework\Models\Journal;
+use Point\Framework\Models\Master\Item;
+use Point\PointManufacture\Models\Formula;
+use Point\PointManufacture\Models\FormulaMaterial;
+use Point\PointManufacture\Models\FormulaProduct;
+use Point\PointManufacture\Models\InputMaterial;
+use Point\PointManufacture\Models\InputProcess;
+use Point\PointManufacture\Models\InputProduct;
+use Point\PointManufacture\Models\OutputProcess;
+use Point\PointManufacture\Models\OutputProduct;
+
+
+class RecalculateOutput extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'dev:recalculate:output';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'recalculate';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $outputs = OutputProcess::join('formulir', 'point_manufacture_output.formulir_id', '=', 'formulir.id')
+            ->where('formulir.approval_status', 1)
+            ->where('formulir.form_status', 1)
+            ->select('point_manufacture_output.*')
+            ->get();
+
+        foreach ($outputs as $output) {
+            Inventory::where('formulir_id', $output->formulir_id)->delete();
+            Journal::where('form_journal_id', $output->formulir_id)->delete();
+
+            $value = Journal::where('form_journal_id', $output->input->formulir_id)->sum('debit');
+
+            $totalQty = 0;
+            \Log::info($output->id . ' = ' . $output->product);
+            foreach ($output->product as $product) {
+                $totalQty += (float) $product->quantity;
+            }
+
+            if ($totalQty == 0) {
+                // \Log::info('zero: ' . $output->id .' / '. $value . ' \ ' . $totalQty);
+                $cogs_product = 0;
+            } else {
+                $cogs_product = $value / $totalQty;
+            }
+
+
+            foreach ($output->product as $product) {
+                $inventory = new Inventory();
+                $inventory->formulir_id = $output->formulir->id;
+                $inventory->form_date = $output->formulir->approval_at;
+                $inventory->warehouse_id = $product->warehouse_id;
+                $inventory->item_id = $product->product_id;
+                $inventory->quantity = $product->quantity;
+                $inventory->price = $cogs_product;
+
+                $inventory_helper = new InventoryHelper($inventory);
+                $inventory_helper->in();
+
+                self::addJournalOutput($inventory, $totalQty);
+            }
+        }
+    }
+
+    public static function addJournalOutput($inventory, $totalInv)
+    {
+        // JOURNAL #2 of #2
+        $output = OutputProcess::where('formulir_id', $inventory->formulir_id)->first();
+        $work_in_process_account_id = JournalHelper::getAccount('manufacture process', 'work in process');
+        $cjournals = Journal::where('form_journal_id', $output->input->formulir_id)
+            ->where('coa_id', $work_in_process_account_id)
+            ->select('journal.*')
+            ->get();
+        
+        $vals = 0;
+
+        foreach ($cjournals as $cjournal) {
+            $journal = new Journal();
+            $journal->form_date = $inventory->formulir->created_at;
+            $journal->coa_id = $work_in_process_account_id;
+            $journal->description = 'Manufacture output process ' . $inventory->item->codeName;
+            $journal->debit = 0;
+            $journal->credit = abs($cjournal->debit) * $inventory->quantity / $totalInv;
+            $journal->form_journal_id = $inventory->formulir->id;
+            $journal->form_reference_id;
+            $journal->subledger_id = $cjournal->subledger_id;
+            $journal->subledger_type = get_class($inventory->item);
+            $journal->save();
+
+            $vals += $journal->credit;
+        }
+
+        // JOURNAL #1 of #2
+        $journal = new Journal();
+        $journal->form_date = $inventory->formulir->created_at;
+        $journal->coa_id = $inventory->item->account_asset_id;
+        $journal->description = 'Manufacture output process ' . $inventory->item->codeName;
+        $journal->debit = $vals;
+        $journal->credit = 0;
+        $journal->form_journal_id = $inventory->formulir->id;
+        $journal->form_reference_id;
+        $journal->subledger_id = $inventory->item_id;
+        $journal->subledger_type = get_class($inventory->item);
+        $journal->save();
+    }
+}
