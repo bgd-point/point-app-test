ffb2141017b59b2cf3364d1323c475e3d7c5add5
Martien Dermawan Tanama <martiendt@gmail.com>
2026-05-05 12:20:32 +0700

    x

--- packages/point/point-purchasing/src/Http/Controllers/Inventory/PurchaseRequisitionController.php
+++ packages/point/point-purchasing/src/Http/Controllers/Inventory/PurchaseRequisitionController.php
@@ -98,11 +98,11 @@ class PurchaseRequisitionController extends Controller
 
         DB::beginTransaction();
 
-        dd('asd');
+        // dd('asd');
         FormulirHelper::isAllowedToCreate('create.point.purchasing.requisition', date_format_db($request['form_date']), []);
-        dd('def');
+        // dd('def');
         $formulir = FormulirHelper::create($request->input(), 'point-purchasing-requisition');
-        dd('qweqw');
+        // dd('qweqw');
         $purchase_requisition = PurchaseRequisitionHelper::create($request, $formulir);
         timeline_publish('create.purchase.requisition', 'added new purchase requisition '  . $purchase_requisition->formulir->form_number);
 
