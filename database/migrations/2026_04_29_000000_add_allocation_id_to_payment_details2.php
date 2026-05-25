c56b265676b8a8d7a67accbd07e31f332dd60116
Martien Dermawan Tanama <martiendt@gmail.com>
2026-05-25 11:13:00 +0700

    x

--- database/migrations/2026_04_29_000000_add_allocation_id_to_payment_details2.php
+++ database/migrations/2026_04_29_000000_add_allocation_id_to_payment_details2.php
@@ -12,9 +12,9 @@ class AddAllocationIdToPaymentDetails2 extends Migration
      */
     public function up()
     {
-        $this->comment('Starting migration...');
+        \Log::info('Starting migration...');
         if (!Schema::hasColumn('point_purchasing_payment_order_detail', 'allocation_id')) {
-            $this->comment('Adding allocation_id column to point_purchasing_payment_order_detail table...');
+            \Log::info('Adding allocation_id column to point_purchasing_payment_order_detail table...');
             Schema::table('point_purchasing_payment_order_detail', function (Blueprint $table) {
                 $table->integer('allocation_id')->unsigned()->default(1)->after('coa_id');
                 $table->foreign('allocation_id', 'pppod_allocation_id_foreign')->references('id')->on('allocation')->onUpdate('restrict')->onDelete('restrict');
