eba8bc972914a7b489f5655845b67aa3e5fd515e
Martien Dermawan Tanama <martiendt@gmail.com>
2026-05-25 10:30:05 +0700

    x

--- app/Console/Commands/RecalculateAll.php
+++ app/Console/Commands/RecalculateAll.php
@@ -60,11 +60,11 @@ class RecalculateAll extends Command
             $prevTotalVal = 0;
             $this->comment('I' . count($items) . ' = ' . $i);
             foreach($list_inventory as $index => $l_inventory) {
-                $l_inventory->total_quantity_all = $prevTotalQty + $l_inventory->quantity;
-                $l_inventory->total_value_all = $prevTotalVal + ($l_inventory->quantity * $l_inventory->price);
                 if ($l_inventory->quantity < 0) {
                     $l_inventory->price = $prevTotalVal / $prevTotalQty;
                 } 
+                $l_inventory->total_quantity_all = $prevTotalQty + $l_inventory->quantity;
+                $l_inventory->total_value_all = $prevTotalVal + ($l_inventory->quantity * $l_inventory->price);
                 if (!$l_inventory->total_quantity_all || $l_inventory->total_quantity_all == 0) {
                     $l_inventory->cogs = 0;
                 } else {
