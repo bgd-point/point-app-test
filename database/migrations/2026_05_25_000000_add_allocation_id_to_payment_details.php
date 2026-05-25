2d64c5673a3c765bef6269adee513a2698467dcc
Martien Dermawan Tanama <martiendt@gmail.com>
2026-05-25 11:19:18 +0700

    x

--- database/migrations/2026_05_25_000000_add_allocation_id_to_payment_details.php
+++ database/migrations/2026_05_25_000000_add_allocation_id_to_payment_details.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddAllocationIdToPaymentDetails2 extends Migration
+class AddAllocationIdToPaymentDetails extends Migration
 {
     /**
      * Run the migrations.
