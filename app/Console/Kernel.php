1b5d1623288ef2f0ebcfb50be5c70d276c23a9de
Martien Dermawan Tanama <martiendt@gmail.com>
2026-06-02 11:48:43 +0700

    x

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends ConsoleKernel
         Commands\Reopname::class,
         Commands\ReHppBBL::class,
         Commands\RecalculateAll::class,
-        Commands\RecalculateAllval::class,
+        Commands\RecalculateAllVal::class,
         Commands\RecalculateBBL::class,
         Commands\RecalculateJournalHpp::class,
         Commands\RecalculateTest::class,
