7438a5703f2a09b8e543420957e02698f2dd3483
Martien Dermawan Tanama <martiendt@gmail.com>
2026-06-12 14:12:48 +0700

    x

--- app/Console/Commands/Recalculate6.php
+++ app/Console/Commands/Recalculate6.php
@@ -110,7 +110,7 @@ class Recalculate6 extends Command
                         }
                     }
                     
-                    $l_inventory->total_value_all = $prevTotalVal + ($l_inventory->quantity * $l_inventory->price);
+                    // $l_inventory->total_value_all = $prevTotalVal + ($l_inventory->quantity * $l_inventory->price);
                     if (!$l_inventory->total_quantity_all || $l_inventory->total_quantity_all == 0) {
                         // $l_inventory->cogs = 0;
                     } else {
