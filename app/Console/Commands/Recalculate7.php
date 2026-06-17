fcdde63f75c7dd00494660de547987d447121a1d
Martien Dermawan Tanama <martiendt@gmail.com>
2026-06-17 12:09:28 +0700

    x

--- app/Console/Commands/Recalculate7.php
+++ app/Console/Commands/Recalculate7.php
@@ -43,8 +43,8 @@ class Recalculate7 extends Command
     {
         $this->comment('handle inventory all');
 
-        // $items = Item::where('id', 607)->get();
-        $items = Item::all();
+        $items = Item::where('id', 678)->get();
+        // $items = Item::all();
 
         foreach ($items as $item) {
             $inventory = Inventory::where('item_id', '=', $item->id)
