7b9c816c928fe3631e32c849b7f2f0e5210c2e60
Martien Dermawan Tanama <martiendt@gmail.com>
2026-08-20 13:07:44 +0700

    x

--- packages/point/point-framework/src/Http/Controllers/Accounting/BalanceSheetController.php
+++ packages/point/point-framework/src/Http/Controllers/Accounting/BalanceSheetController.php
@@ -16,10 +16,11 @@ class BalanceSheetController extends Controller
     public function index()
     {
         $date_from = '2000-01-01 00:00:00';
-        $date_to = date('Y-m-d 23:59:59');
 
-        $month_to = request('month_to');
-        $year_to = request('year_to');
+        $default_date = now()->subMonth();
+
+        $month_to = request('month_to', $default_date->month);
+        $year_to = request('year_to', $default_date->year);
 
         if ($month_to == date('n') && $year_to == date('Y')) {
             return back()->withErrors([
@@ -28,22 +29,30 @@ class BalanceSheetController extends Controller
         }
 
         $view = view('framework::app.accounting.balance-sheet.index');
+
         $view->coa_asset = CoaPosition::find(1);
         $view->coa_liability = CoaPosition::find(2);
         $view->coa_equity = CoaPosition::find(3);
+
         $view->total_asset = 0;
         $view->total_liability = 0;
         $view->total_equity = 0;
+
         $view->date_from = $date_from;
-        $view->month = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-        $view->date_to = (app('request')->input('month_to') && app('request')->input('year_to'))
-            ? date(
-                'Y-m-t 23:59:59',
-                strtotime(
-                    app('request')->input('year_to') . '-' . app('request')->input('month_to') . '-01'
-                )
-            )
-            : date('Y-m-t 23:59:59');
+
+        $view->month = [
+            'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN',
+            'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'
+        ];
+
+        $view->month_to = $month_to;
+        $view->year_to = $year_to;
+
+        $view->date_to = date(
+            'Y-m-t 23:59:59',
+            strtotime($year_to . '-' . $month_to . '-01')
+        );
+
         return $view;
     }
 
@@ -68,7 +77,7 @@ class BalanceSheetController extends Controller
                 'date_to' => 'The selected month and year cannot be the current month and year.',
             ]);
         }
-        
+
         \Excel::create($file_name, function ($excel) use ($date_from, $date_to) {
             $excel->sheet('Balance Sheet', function ($sheet) use ($date_from, $date_to) {
                 $data = array(
