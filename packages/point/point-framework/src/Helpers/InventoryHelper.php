18354527fcfa9823cc3b940231eecc7a79437d83
Martien Dermawan Tanama <martiendt@gmail.com>
2026-08-21 12:15:47 +0700

    x

--- packages/point/point-framework/src/Helpers/InventoryHelper.php
+++ packages/point/point-framework/src/Helpers/InventoryHelper.php
@@ -467,7 +467,7 @@ class InventoryHelper
         }
 
         if (!$last || (float) $last->total_quantity < abs($this->inventory->quantity)) {
-            throw new PointException('STOCK ' . $this->inventory->item->name . ' NOT AVAILABLE');
+            throw new PointException('STOCK ' . $this->inventory->item->name . ' NOT AVAILABLE (' . $last->total_quantity .'<' . $this->inventory->quantity . ')');
         }
 
         $this->inventory->cogs = $cogsVal;
