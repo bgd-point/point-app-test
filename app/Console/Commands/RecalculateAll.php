27b76a979b2d7c71cc3d40781df05b4e463ec3e6
Martien Dermawan Tanama <martiendt@gmail.com>
2026-08-24 11:12:42 +0700

    x

--- app/Console/Commands/RecalculateAll.php
+++ app/Console/Commands/RecalculateAll.php
@@ -59,7 +59,7 @@ class RecalculateAll extends Command
             foreach($list_inventory as $index => $l_inventory) {
                 $l_inventory->total_value = $l_inventory->total_quantity * $l_inventory->cogs;
                 $l_inventory->recalculate = 0;
-                if ($item_id == 851) {
+                if ($item->id == 851) {
                     $this->comment($l_inventory);
                 }
                 $l_inventory->save();
