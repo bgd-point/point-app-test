84dd8d55759a629c3d0ca9bbba1186b3702d3209
Martien Dermawan Tanama <martiendt@gmail.com>
2026-09-18 09:24:38 +0700

    x

--- app/Console/Commands/RecalculateMJ.php
+++ app/Console/Commands/RecalculateMJ.php
@@ -16,6 +16,8 @@ use Point\Framework\Helpers\JournalHelper;
 use Point\PointInventory\Models\StockCorrection\StockCorrection;
 use Point\PointInventory\Models\StockCorrection\StockCorrectionItem;
 use Point\PointInventory\Helpers\StockCorrectionHelper;
+use Point\PointAccounting\Models\MemoJournal;
+use Point\PointAccounting\Helpers\MemoJournalHelper;
 
 class RecalculateMJ extends Command
 {
@@ -68,9 +70,67 @@ class RecalculateMJ extends Command
             ->orderBy('journal.subledger_id')
             ->get();
 
-            foreach ($journals as $journal) {
-              $this->comment($journal->coa_name . ' > ' . $journal->item_name . ' = ' . $journal->balance);
-            }
+          $form_date = '2026-07-31 23:59:59';
+          $form_number = FormulirHelper::number('point-accounting-memo-journal', $form_date);
+
+          $formulir = new Formulir;
+          $formulir->form_date = $form_date;
+          $formulir->created_at = $form_date;
+          $formulir->updated_at = $form_date;
+          $formulir->form_number = $form_number['form_number'];
+          $formulir->form_raw_number = $form_number['raw'];
+          $formulir->notes = 'Koreksi Journal Sediaan 2026-07-31';
+          $formulir->approval_to = 1;
+          $formulir->approval_status = 1;
+          $formulir->approval_message = '';
+          $formulir->created_by = 1;
+          $formulir->updated_by = 1;
+          if (!$formulir->save()) {
+              gritter_error('create has been failed', false);
+          }
+
+          $memo_journal = new MemoJournal;
+          $memo_journal->formulir_id = $formulir_id;
+          $memo_journal->debit = 0;
+          $memo_journal->credit = 0;
+          $memo_journal->save();
+
+          $total = 0;
+          foreach ($corrections as $correction) {
+            $this->comment($correction->coa_name . ' > ' . $correction->item_name . ' = ' . $correction->balance);
+
+            $memo_journal_detail = new MemoJournalDetail;
+            $memo_journal_detail->memo_journal_id = $memo_journal->id;
+            $memo_journal_detail->coa_id = $journal->coa_id;
+            $memo_journal_detail->description = 'Koreksi Journal Sediaan 2026-07-31';
+            $memo_journal_detail->debit = 0;
+            $memo_journal_detail->credit = $correction->balance;
+            $memo_journal_detail->form_journal_id = $formulir->id;
+            $memo_journal_detail->form_reference_id = '';
+            $memo_journal_detail->subledger_id = $correction->subledger_id;
+            $memo_journal_detail->subledger_type = 'Point\Framework\Models\Master\Item';
+            $memo_journal_detail->save();
+
+            $memo_journal_detail = new MemoJournalDetail;
+            $memo_journal_detail->memo_journal_id = $memo_journal->id;
+            $memo_journal_detail->coa_id = 278;
+            $memo_journal_detail->description = 'Koreksi Journal Sediaan 2026-07-31';
+            $memo_journal_detail->debit = $correction->balance;
+            $memo_journal_detail->credit = 0;
+            $memo_journal_detail->form_journal_id = $formulir->id;
+            $memo_journal_detail->form_reference_id = '';
+            $memo_journal_detail->subledger_id = $correction->subledger_id;
+            $memo_journal_detail->subledger_type = 'Point\Framework\Models\Master\Item';
+            $memo_journal_detail->save();
+
+            $total += $correction->balance;
+          }
+
+          $memo_journal->debit = $total;
+          $memo_journal->credit = $total;
+          $memo_journal->save();
+
+          MemoJournalHelper::addToJournal($memo_journal);
         }
 
         \DB::commit();
