3c2c5a4b54e201830435c5a62904fb106e914e48
Martien Dermawan Tanama <martiendt@gmail.com>
2026-09-22 09:09:43 +0700

    x

--- app/Console/Commands/RecalculateMJ.php
+++ app/Console/Commands/RecalculateMJ.php
@@ -93,6 +93,8 @@ class RecalculateMJ extends Command
             ->orderBy('journal.subledger_id')
             ->get();
 
+          
+
           $form_date = '2026-07-31 23:59:59';
           $form_number = FormulirHelper::number('point-accounting-memo-journal', $form_date);
 
@@ -122,17 +124,37 @@ class RecalculateMJ extends Command
           foreach ($corrections as $correction) {
             $this->comment($correction->coa_name . ' > ' . $correction->subledger_id . ' = ' . $correction->balance);
 
+            $balance = (float) $correction->balance;
+
+            if ($balance == 0) {
+                continue;
+            }
+
+            if ($balance > 0) {
+                $inventoryDebit = 0;
+                $inventoryCredit = $balance;
+
+                $differenceDebit = $balance;
+                $differenceCredit = 0;
+            } else {
+                $inventoryDebit = abs($balance);
+                $inventoryCredit = 0;
+
+                $differenceDebit = 0;
+                $differenceCredit = abs($balance);
+            }
+
             // COA SEDIAAN
             $memo_journal_detail = new MemoJournalDetail;
             $memo_journal_detail->memo_journal_id = $memo_journal->id;
             $memo_journal_detail->coa_id = $correction->coa_id;
             $memo_journal_detail->description = 'Koreksi Journal Sediaan ' . $correction->coa_name . ' 2026-07-31';
-            $memo_journal_detail->debit = 0;
-            $memo_journal_detail->credit = $correction->balance;
+            $memo_journal_detail->debit = $inventoryDebit;
+            $memo_journal_detail->credit = $inventoryCredit;
             $memo_journal_detail->form_journal_id = $formulir->id;
             $memo_journal_detail->form_reference_id = null;
-            $memo_journal_detail->subledger_id = $correction->subledger_id ?? NULL;
-            $memo_journal_detail->subledger_type = $correction->subledger_id ? 'Point\Framework\Models\Master\Item' : NULL;
+            $memo_journal_detail->subledger_id = $correction->subledger_id;
+            $memo_journal_detail->subledger_type = 'Point\Framework\Models\Master\Item';
             $memo_journal_detail->save();
 
             // COA SELISIH KOREKSI
@@ -140,15 +162,15 @@ class RecalculateMJ extends Command
             $memo_journal_detail->memo_journal_id = $memo_journal->id;
             $memo_journal_detail->coa_id = 50;
             $memo_journal_detail->description = 'Koreksi Journal Sediaan ' . $correction->coa_name . ' 2026-07-31';
-            $memo_journal_detail->debit = $correction->balance;
-            $memo_journal_detail->credit = 0;
+            $memo_journal_detail->debit = $differenceDebit;
+            $memo_journal_detail->credit = $differenceCredit;
             $memo_journal_detail->form_journal_id = $formulir->id;
             $memo_journal_detail->form_reference_id = null;
-            $memo_journal_detail->subledger_id = $correction->subledger_id ?? NULL;
-            $memo_journal_detail->subledger_type = $correction->subledger_id ? 'Point\Framework\Models\Master\Item' : NULL;
+            $memo_journal_detail->subledger_id = $correction->subledger_id;
+            $memo_journal_detail->subledger_type = 'Point\Framework\Models\Master\Item';
             $memo_journal_detail->save();
 
-            $total += $correction->balance;
+            $total += $balance;
           }
 
           $memo_journal->debit = $total;
